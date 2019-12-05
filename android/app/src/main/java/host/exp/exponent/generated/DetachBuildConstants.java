17d359fcb450afe02704b31e08f483db4c9df57a
Badi Ifaoui <badi1wow@gmail.com>
2019-12-05 18:57:40 +0100

    updated: app assets

--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "";
+  public static final String DEVELOPMENT_URL = "yukon://192.168.8.101:19000";
 
 }
