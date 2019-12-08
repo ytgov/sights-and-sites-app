e548d86906c5a7487ae5615564ac22579adf4fe3
Badi Ifaoui <badi1wow@gmail.com>
2019-12-08 12:44:21 +0100

    added: mapbox

--- src/stacks/TestScreen.js
+++ src/stacks/TestScreen.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import {Container, Content, Text, View} from 'native-base';
-// import MapboxGL from '@react-native-mapbox-gl/maps';
+import {Container, Content} from 'native-base';
+import MapboxGL from '@react-native-mapbox-gl/maps';
+
+MapboxGL.setAccessToken('pk.eyJ1IjoiMjQ3bGFicyIsImEiOiJjankwNjc0Y2IwYWZrM2RwanZzcG92MnFoIn0.YahNe0xRjc58mSA5CveCSg');
 
 
-export default class TestScreen extends React.Component{
+export default class TestScreen extends React.Component {
 
     render() {
         return (
-            <Container>
-                <Content>
-                    <View style={{padding: 20}}>
-                        <Text>
-                            Hello From Test Screen
-                        </Text>
-                    </View>
-
-
-                </Content>
-            </Container>
+            <MapboxGL.MapView style={{flex: 1}}></MapboxGL.MapView>
         )
     }
 
---
+++ src/stacks/listing/containers/map-view/old-map-view.container.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import MapView, {Marker} from 'react-native-maps';
+import {Dimensions} from 'react-native';
+
+const {width, height} = Dimensions.get('window');
+
+class MapViewContainer extends React.Component {
+    state = {
+        places: [
+            {
+                site_id: 1728,
+                changed_ts: 1531850203,
+                site_name: 'Tagish Bridge',
+                site_types: [
+                    'Recreation site (day use only)'
+                ],
+                regions: [
+                    'Southern Lakes'
+                ],
+                latitude: 60.316,
+                longitude: 134.27,
+                highway_name: '',
+                highway_km: 0,
+                secondary_road_name: '',
+                secondary_road_km: 0,
+                community: '',
+                warning: '',
+                site_directions: '',
+                site_description: '<ul> <li>The Tagish River, also known as Six Mile River, enters Marsh Lake here.</li> <li>Tagish means &ldquo;break-up of ice.&rdquo;</li> <li>Swans and waterfowl rest here in March.</li> <li>The Tagish Bridge is popular place to fish. You can get there from the recreation site.</li> </ul> ',
+                images: [
+                    {}
+                ],
+                fn_traditional_territories: [],
+                rest_stop: false,
+                historic_site: false,
+                artifact: false,
+                number_of_campsites: 0,
+                campsite_wc_accessible: false,
+                campsite_walk_in: false,
+                campsite_pull_through: false,
+                outhouse: true,
+                outhouse_accessible: true,
+                trail: false,
+                picnic_table: false,
+                picnic_shelter: false,
+                fire_ring: false,
+                garbage_can: true,
+                recycle_bin: false,
+                food_storage: false,
+                playground: false,
+                boat_launch: true,
+                dock: false,
+                water_supply: false,
+                beach: false,
+                change_room: false
+            }
+        ]
+    }
+
+    componentDidMount() {
+
+    }
+
+    renderMarkers = () => {
+        const {data} = this.props
+        let results = data.map((marker, i) => {
+            const latitude = marker.latitude
+            const longitude = -marker.longitude
+            let coords = {
+                latitude,
+                longitude
+            }
+
+
+            return <Marker
+                key={i}
+                coordinate={coords}
+                title={marker.site_name}
+            />
+        })
+        return results
+    }
+
+    render() {
+        return (
+            <MapView
+                initialRegion={{
+                    latitude: 63.389423,
+                    longitude: -136.714739,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}
+                style={{width, height, flex: 1}}>
+                {this.renderMarkers()}
+            </MapView>
+        )
+    }
+}
+
+export default MapViewContainer;
