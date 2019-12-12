26f1180d75ed2778330286c82af6a7a1c6434aaf
Badi Ifaoui <badi1wow@gmail.com>
2019-12-12 15:41:29 +0100

    updated: android first build

--- src/load-resources-async.js
+++ src/load-resources-async.js
@@ -22,7 +22,9 @@ export default async () => Promise.all([
 
     cacheImages([
         // Common
-        require('../assets/images/pin.png'),
+        require('../assets/images/pin.png' +
+            '' +
+            ''),
         require('../assets/common/logo-circle.png'),
         require('../assets/common/logo.png'),
         require('../assets/common/logo-white.png'),
