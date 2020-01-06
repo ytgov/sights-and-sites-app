3b75ffa559616ac181e27566c662ca267b0fcfd7
Badi Ifaoui <badi1wow@gmail.com>
2020-01-06 17:44:10 +0100

    updated: map fox IOS

--- android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
@@ -40,10 +40,11 @@ public class AppConstants {
         
         
         
+        
         // ADD EMBEDDED RESPONSES HERE
         // START EMBEDDED RESPONSES
         embeddedResponses.add(new Constants.EmbeddedResponse("https://exp.host/@247labs/yukon", "assets://shell-app-manifest.json", "application/json"));
-        embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40247labs%2Fyukon%2F1.0.0%2F2ff6ad16982e7c8c6f7c583cc0cf9039-35.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
+        embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40247labs%2Fyukon%2F1.0.0%2F0e95a2c12f9eb324452a7a13c6715bcf-35.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
         // END EMBEDDED RESPONSES
     EMBEDDED_RESPONSES = embeddedResponses;
   }
--- src/stacks/listing/containers/map-view/ios-map-view.container.js
+++ src/stacks/listing/containers/map-view/ios-map-view.container.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Dimensions, Image} from 'react-native';
+import {ActivityIndicator, Dimensions, Image, Text, View} from 'react-native';
 import MapboxGL from '@react-native-mapbox-gl/maps';
 
 MapboxGL.setAccessToken('pk.eyJ1IjoiMjQ3bGFicyIsImEiOiJjankwNjc0Y2IwYWZrM2RwanZzcG92MnFoIn0.YahNe0xRjc58mSA5CveCSg');
@@ -20,14 +20,15 @@ class IosMapViewContainer extends React.Component {
         data.map(marker => {
             items.push(
                 <MapboxGL.PointAnnotation
-                    key={marker.site_id.toString()+'-key'}
-                    id={marker.site_id.toString()+'-id'}
+                    key={marker.site_id.toString() + '-key'}
+                    id={marker.site_id.toString() + '-id'}
                     coordinate={[marker.longitude, marker.latitude]}
                     style={{zIndex: 10000}}
                     title={marker.site_name}
                 >
-                    <Image style={{width: 20, height: 20, zIndex: 1000}} resizeMode="contain" source={require('../../../../../assets/images/pin.png')}/>
-                    <MapboxGL.Callout title={marker.site_name} />
+                    <Image style={{width: 20, height: 20, zIndex: 1000}} resizeMode="contain"
+                           source={require('../../../../../assets/images/pin.png')}/>
+                    <MapboxGL.Callout title={marker.site_name}/>
                 </MapboxGL.PointAnnotation>,
             )
         });
@@ -35,16 +36,53 @@ class IosMapViewContainer extends React.Component {
 
     };
 
+    moveTheMap(map) {
+        map.moveTo([
+            63.389423,
+            -136.714739
+        ], 3000)
+    }
+
     render() {
+
+        const initialRegion = {
+            latitude: 63.389423,
+            longitude: -136.714739,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+            zoomLevel: 2,
+
+        };
+        const {data} = this.props;
+
+
+        if (!data.length) {
+            return (<View style={{
+                    width,
+                    height: (height - 200),
+                    flex: 1,
+                    justifyContent: 'center',
+                    textAlign: 'center'
+                }}>
+                    <Text style={{
+                        color: '#FFF',
+                        fontSize: 24,
+                        textAlign: 'center',
+                        paddingBottom: 20,
+                    }}>Loading the map ...</Text>
+                    <ActivityIndicator style={{}} size="large" color="#FFF"/>
+                </View>
+            )
+        }
         return (
             <MapboxGL.MapView
-                // initialRegion={{
-                //     latitude: 63.389423,
-                //     longitude: -136.714739,
-                //     latitudeDelta: 0.0922,
-                //     longitudeDelta: 0.0421,
-                // }}
+
+
                 style={{width, height, flex: 1}}>
+                <MapboxGL.Camera
+                    centerCoordinate={[initialRegion.longitude, initialRegion.latitude]}
+                    zoomLevel={initialRegion.zoomLevel}
+                />
                 {this.renderMarkers()}
             </MapboxGL.MapView>
         )
