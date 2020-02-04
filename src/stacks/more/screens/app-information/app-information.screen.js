18a496106c5a701c0f8ef2deca36b9b4be3ca731
Badi Ifaoui <badi1wow@gmail.com>
2020-02-04 09:36:50 +0100

    updated: app info

--- src/stacks/more/screens/app-information/app-information.screen.js
+++ src/stacks/more/screens/app-information/app-information.screen.js
@@ -16,6 +16,102 @@ class AppInformationScreen extends React.Component {
 
     render() {
         const {navigation} = this.props;
+        const fr_data = [
+            {
+                title: 'Services de location',
+                content: '',
+                QA: [
+                    {
+                        q: 'Pourquoi l\'application souhaite-t-elle accéder à ma position?',
+                        a: `The app uses the location provided by your device to show you helpful results, like nearby sites.
+This app does not store the location, it is only used within your device. You can enable location settings within the app or on your device.`
+                    },
+                    {
+                        q: 'I don’t want to enable location services',
+                        a: 'No problem! You can still search and filter sites, however you will not be able to navigate to these sites or see nearby sites within the app.'
+                    }
+                ]
+
+            },
+            {
+                title: 'Mobile Data',
+                content: ' If your device is connected to the internet, it will use mobile data to update the app if required.',
+                QA: [
+                    {
+                        q: 'Can I use the app offline?',
+                        a: `The app functions entirely offline. The maps are downloaded to your device when you give the app permission to do so.`
+                    },
+                    {
+                        q: 'When is the information in the app updated? ',
+                        a:
+                            `The app is updated periodically with new data when it becomes available. This includes:
+
+\u2022 New sites
+\u2022 Existing site services
+\u2022 Directions
+\u2022 Map details
+            `
+                    }
+                ]
+            },
+            {
+                title: 'Privacy Policy',
+                content: `The Government of Yukon is committed to protecting your privacy. The collection, use, disclosure, retention, and disposal of information collected via Government of Yukon online properties, such as our websites is done in compliance with the Access to Information and Protection of Privacy Act and the Archives Act.
+
+The purpose of this privacy statement is to inform you of the personal information that may be collected from you when you interact with the Government of Yukon online.`
+            },
+            {
+                title: 'Terms & Conditions',
+                content: '',
+                QA: [
+                    {
+                        q: 'Avis de non responsabilité',
+                        a: 'Ces renseignements sont distribués « tels quels » et ne présentent aucune garantie exprimée ou implicite. L’information peut être utilisée à la condition qu’il soit formellement entendu que ni le gouvernement, ni ses ministres, employés ou agents ne seront responsables des pertes ou des dommages de quelque nature que ce soit qui pourraient résulter de l’utilisation des renseignements fournis dans cette application ou sur les sites externes pour lesquels elle offre des liens.',
+                    },
+                    {
+                        q: 'Avis de confidentialité',
+                        a: 'Le gouvernement du Yukon s’engage à protéger vos renseignements personnels. La collecte, l’utilisation, la divulgation, la conservation et l’élimination de l’information recueillie à partir des ressources en ligne du gouvernement du Yukon telles que les sites Web et les applications mobiles sont faits en conformité avec la Loi sur l’accès à l’information et la protection des renseignements personnels et la Loi sur les archives.\n \n' +
+                            'Le présent avis de confidentialité a pour objet de vous faire savoir quels renseignements personnels pourraient être recueillis lorsque vous utilisez cette application.',
+                    },
+                    {
+                        q: 'Collecte de renseignements personnels',
+                        a: 'La collecte de renseignements personnels recueillis à partir des sites Web du gouvernement est effectuée conformément au paragraphe 29c) de la Loi sur l’accès à l’information et la protection de la vie privée aux fins indiquées ci-après : \n',
+                        list: [
+                            {
+                                q: '1. Communiquer avec le ministère du Tourisme et de la Culture, gouvernement du Yukon',
+                                a: 'Si vous choisissez d’envoyer par courriel une demande de renseignements ou des commentaires généraux sur l’application mobile, vos renseignements personnels peuvent être utilisés pour répondre à votre courriel.\n' +
+                                    'Les courriels et autres méthodes électroniques utilisées pour communiquer avec le gouvernement du Yukon ne sont pas protégés, à moins d’indication contraire, clairement énoncée. Il est donc recommandé de ne pas transmettre de renseignements personnels de nature délicate par ces moyens.'
+                            },
+                            {
+                                q: '2. Fichier de journalisation',
+                                a: 'Nous recueillons les informations que votre appareil envoie lorsque vous utilisez notre application mobile (« fichier de journalisation »).'
+                            },
+                            {
+                                a: 'Le fichier de journalisation contient des informations telles que :',
+                                items: [
+                                    'l’adresse de protocole Internet (adresse IP) de votre ordinateur,',
+                                    'la date et l’heure de la demande.'
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        q: 'Renseignements :',
+                        a: 'Pour toute question, veuillez contacter la Section des lieux d’intérêt historique, ministère du Tourisme et de la Culture, gouvernement du Yukon.\n',
+                        links: [
+                            {
+                                url: 'mailto:Heritage.planning@gov.yk.ca',
+                                title: 'Heritage.planning@gov.yk.ca'
+                            },
+                            {
+                                url: 'tel:1-867-667-5386',
+                                title: '1-867-667-5386'
+                            }
+                        ]
+                    }
+                ],
+            }
+        ];
         const data = [
             {
                 title: 'Location Services',
@@ -147,7 +243,7 @@ The purpose of this privacy statement is to inform you of the personal informati
                                 </View>
 
                                 <View>
-                                    <AccordionCustom data={data}/>
+                                    <AccordionCustom data={fr_data}/>
                                 </View>
                             </View>
                         </Content>
