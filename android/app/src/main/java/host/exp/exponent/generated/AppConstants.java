809de435df3b594357912b4b6d3be7bb8094bf4a
Badi Ifaoui <badi1wow@gmail.com>
2020-03-19 16:34:44 +0100

    added: amenties modal

--- android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
@@ -99,10 +99,12 @@ public class AppConstants {
         
         
         
+        
+        
         // ADD EMBEDDED RESPONSES HERE
         // START EMBEDDED RESPONSES
         embeddedResponses.add(new Constants.EmbeddedResponse("https://exp.host/@247labs/yukon", "assets://shell-app-manifest.json", "application/json"));
-        embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40247labs%2Fyukon%2F1.0.0%2F8163547edd952a6c1e101b43f77ab109-35.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
+        embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40247labs%2Fyukon%2F1.0.0%2Fd2a2c1a9b6ddf8fa096adcbcd38a5581-35.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
         // END EMBEDDED RESPONSES
     EMBEDDED_RESPONSES = embeddedResponses;
   }
--- src/stacks/listing/components/site-amenties/site-amenties.component.js
+++ src/stacks/listing/components/site-amenties/site-amenties.component.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Dimensions, Image, View} from 'react-native';
+import {Dimensions, Image, Text, TouchableOpacity, View} from 'react-native';
 import {SITE_AMENTIES} from '../../../../config';
 import SiteAmentiesStyles from './site-amenties.styles';
 import SiteType from '../../../../types/site.type';
+import Modal from 'react-native-modal';
+import {H3} from '../../../../theme/typings';
+import {COLORS} from '../../../../theme/config';
+import {Button} from "native-base";
 
 const {width} = Dimensions.get('window');
 
 class SiteAmenties extends React.Component {
+    state = {
+        modal: false
+    };
 
     getAmenties(item) {
         let items = [];
@@ -27,23 +34,68 @@ class SiteAmenties extends React.Component {
         return items;
     }
 
+    renderModal(site) {
+
+        return (
+            <Modal
+                isVisible={this.state.modal}
+                onBackdropPress={() => this.setState({modal: false})}
+                deviceWidth={Dimensions.get('window').width}
+            >
+                <View style={{
+                    width: '100%',
+                    backgroundColor: 'white',
+                    padding: 30,
+                    justifyContent: 'center',
+                    borderRadius: 4,
+                    borderColor: 'rgba(0, 0, 0, 0.1)',
+                }}>
+                    {
+                        this.getAmenties(site).map(item => (
+                            <View key={`modal-wrapper-${item.id}`} style={{
+                                flexDirection: 'row',
+                                marginBottom: 10,
+                                maxWidth: '85%',
+                            }}>
+                                <Image source={item.imageInactive} key={`modal-image-${item.id}`}
+                                       style={SiteAmentiesStyles.amenitiesListIcon}
+                                       resizeMode='contain'/>
+                                <H3 style={{paddingTop: 10, paddingLeft: 10, width: '100%'}} black>{item[this.props.locale]}</H3>
+                            </View>
+                        ))
+                    }
+
+                    <Button block style={{backgroundColor: COLORS.accent}}
+                            onPress={() => this.setState({modal: false})}>
+                        <Text style={{color: '#FFF'}}>Close</Text>
+                    </Button>
+                </View>
+            </Modal>
+        )
+    }
+
     render() {
         const {item} = this.props;
         return (
-            <View style={SiteAmentiesStyles.amenitiesList}>
-                {
-                    this.getAmenties(item).chunk_inefficient(Math.floor(width / 50)).map((items, index) => (
-                        <View key={'amen-t-'+index} style={{flexDirection: 'row', width}}>
-                            {
-                                items.map(item => (
-                                    <Image source={item.imageInactive} key={item.id} style={SiteAmentiesStyles.amenitiesListIcon}
-                                           resizeMode='contain'/>
-                                ))
-                            }
-                        </View>
-                    ))
-                }
+            <View>
+                {this.renderModal(item)}
+                <TouchableOpacity onPress={() => this.setState({modal: true})}
+                                  style={SiteAmentiesStyles.amenitiesList}>
+                    {
+                        this.getAmenties(item).chunk_inefficient(Math.floor(width / 50)).map((items, index) => (
+                            <View key={'amen-t-' + index} style={{flexDirection: 'row', width}}>
+                                {
+                                    items.map(item => (
+                                        <Image source={item.imageInactive} key={item.id}
+                                               style={SiteAmentiesStyles.amenitiesListIcon}
+                                               resizeMode='contain'/>
+                                    ))
+                                }
+                            </View>
+                        ))
+                    }
 
+                </TouchableOpacity>
             </View>
         )
     }
