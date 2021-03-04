41e40fa06e9cbb81e89a96ea1cee98e9c7717f8c
Robert Ngo <robert.ngo@evolvingweb.ca>
2021-03-04 17:23:18 -0500

    refs #26480: refactor side menu.

--- src/shared/components/navigation/side-menu.js
+++ src/shared/components/navigation/side-menu.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Image, ScrollView} from 'react-native';
+import {View, Image, ScrollView, StyleSheet} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {DrawerItems} from 'react-navigation-drawer';
 import {withNavigation} from 'react-navigation';
@@ -16,13 +16,14 @@ const SideMenu = (props) => {
                 style={{flex: 1}}
                 forceInset={{top: 'always', horizontal: 'never'}}>
 
-                <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between', marginHorizontal: 16, marginTop: 16, marginBottom: 32}}>
-                    <Image source={YukonLogo} style={{ height: 40, width: 105, resizeMode: 'contain'}}></Image>
+                <View style={styles.header}>
+                    <Image source={YukonLogo}
+                           style={styles.logo} />
 
                     <MaterialCommunityIcons.Button
                         name="close"
                         size={24}
-                        color="#aeaeae"
+                        color="black"
                         backgroundColor={'transparent'}
                         underlayColor={'transparent'}
                         iconStyle={{marginRight: 0}}
@@ -41,3 +42,19 @@ const SideMenu = (props) => {
 }
 
 export default withNavigation(SideMenu)
+
+const styles = StyleSheet.create({
+    header: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginHorizontal: 16,
+        marginTop: 16,
+        marginBottom: 32
+    },
+    logo: {
+        height: 40,
+        width: 105,
+        resizeMode: 'contain'
+    }
+})
