514990a9150854a0c9087bc141c7c76aa30133d5
Robert Ngo <robert.ngo@evolvingweb.ca>
2021-03-05 12:44:27 -0500

    refs #26480: styling sidemenu.
    Former-commit-id: 8c6900b05cc9af36b5d2ddce4f7d173da3005c98

---
+++ src/shared/components/side-menu/side-menu.styles.js
@@ -0,0 +1,47 @@
+import {StyleSheet} from 'react-native';
+import {YUKON_COLORS} from '../../../theme/config';
+import {YUKON_FONTS} from '../../../theme/typings';
+
+const styles = StyleSheet.create({
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginHorizontal: 16,
+        marginTop: 16,
+        marginBottom: 32,
+    },
+    logo: {
+        height: 40,
+        width: 105,
+        resizeMode: 'contain'
+    },
+    body: {
+        flex: 1,
+        justifyContent: 'space-between'
+    },
+    languageWraper: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 12,
+    },
+    langcode: {
+        height: 46,
+        width: 46,
+        backgroundColor: YUKON_COLORS.primary_200,
+        borderRadius: 23,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginRight: 12,
+    },
+    langcodeText: {
+        color: 'white',
+        fontSize: 16,
+        fontFamily: YUKON_FONTS.MONTSERRAT_BOLD
+    },
+    menuLabel: {
+        fontSize: 16,
+        fontFamily: YUKON_FONTS.MONTSERRAT_BOLD
+    }
+})
+
+export default styles
