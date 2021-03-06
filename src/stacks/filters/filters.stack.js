d4817191fb9f25d9ae50de5811396553e6087486
Robert Ngo <robert.ngo@evolvingweb.ca>
2021-03-05 19:02:48 -0500

    refs #26469: add screen filter by region to stack.
    Former-commit-id: ee0467d2cdf837f326cf2b0993a54dc98b3d7e04

--- src/stacks/filters/filters.stack.js
+++ src/stacks/filters/filters.stack.js
@@ -3,6 +3,7 @@ import routes from '../../navigation/routes';
 
 import FilterIndexScreen from './screens/index/index.screen';
 import FilterBySiteTypeScreen from './screens/by-site-type/by-site-type.screen';
+import FilterByRegionScreen from './screens/by-region/by-region.sceen';
 
 const FilterStack = createStackNavigator({
     [routes.SCREEN_FILTER_INDEX]: {
@@ -10,6 +11,9 @@ const FilterStack = createStackNavigator({
     },
     [routes.SCREEN_FILTER_SITE_TYPE]: {
         screen: FilterBySiteTypeScreen
+    },
+    [routes.SCREEN_FILTER_REGION]: {
+        screen: FilterByRegionScreen
     }
 }, {
     initialRouteName: routes.SCREEN_FILTER_INDEX,
