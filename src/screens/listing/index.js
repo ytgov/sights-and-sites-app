60baabba583ffa7da07cf0d4bdc4b2f0b67c5bed
Robert Ngo <robert.ngo@evolvingweb.ca>
2021-03-09 17:22:12 -0500

    refs #26468: add placeholder for new screens.
    Former-commit-id: 3908f5068714f6efff699c0a9081046f31d617dd

---
+++ src/screens/listing/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text} from 'react-native';
+import ScreenWrapper from '../../components/screenWrapper';
+
+const ListingScreen = () => {
+    return (
+        <ScreenWrapper>
+            <Text>ListingScreen</Text>
+        </ScreenWrapper>
+    );
+};
+
+export default ListingScreen;
