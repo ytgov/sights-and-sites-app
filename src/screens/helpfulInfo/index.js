f61f247520ca4cf2fbe0934e30f9c8d2948b672c
Robert Ngo <robert.ngo@evolvingweb.ca>
2021-03-09 17:22:12 -0500

    refs #26468: add placeholder for new screens.
    Former-commit-id: 4f5cd9e88070bcec6d6b4cb82878b442932211cf

---
+++ src/screens/helpfulInfo/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text} from 'react-native';
+import ScreenWrapper from '../../components/screenWrapper';
+
+const HelpfulInfoScreen = () => {
+    return (
+        <ScreenWrapper>
+            <Text>HelpfulInfoScreen</Text>
+        </ScreenWrapper>
+    );
+};
+
+export default HelpfulInfoScreen;
