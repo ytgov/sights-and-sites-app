8dea5355c082cb2a7995fd9d0ff93f7512920a66
Robert Ngo <robert.ngo@evolvingweb.ca>
2021-03-10 00:23:15 -0500

    refs #26468: add screen Filter by Type.

--- src/components/filters/title/index.js
+++ src/components/filters/title/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import {View, Image, TouchableWithoutFeedback} from 'react-native';
 import {withNavigation} from 'react-navigation';
 
-import {H2} from '../../../../theme/typings';
+import {H2} from '../../../theme/typings';
 import styles from './title.styles';
 
 const arrow = require('./images/arrow-back.png');
