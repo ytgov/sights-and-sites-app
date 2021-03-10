66272e70535b1bea20c24970b23c956385fa243e
Robert Ngo <robert.ngo@evolvingweb.ca>
2021-03-10 00:23:15 -0500

    refs #26468: add screen Filter by Type.
    Former-commit-id: 8dea5355c082cb2a7995fd9d0ff93f7512920a66

--- src/screens/filters/filterByType.js
+++ src/screens/filters/filterByType.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ScreenWrapper from '../../components/screenWrapper';
-import {Text} from 'react-native';
+import {FilterHeader} from '../../theme/layout';
+import Title from '../../components/filters/title';
+import ListTileCheckbox from '../../components/filters/listTile/listTileCheckbox';
+
+const bgDefault = require('./images/bg-type-history-culture.jpg');
+
+const filters = [
+    { label: 'Camping', icon: require('./images/camping.png'), background: require('./images/bg-type-camping.jpg') },
+    { label: 'Recreation', icon: require('./images/recreation.png'), background: require('./images/bg-type-recreation.jpg') },
+    { label: 'Wildlife & landscape', icon: require('./images/wildlife.png'), background: require('./images/bg-type-wildlife.jpg') },
+    { label: 'History & culture', icon: require('./images/history-culture.png'), background: require('./images/bg-type-history-culture.jpg') },
+]
 
 const FilterByTypeScreen = () => {
+    const [background, setBackground] = useState(bgDefault);
+
     return (
-        <ScreenWrapper>
-            <Text>FilterByTypeScreen</Text>
+        <ScreenWrapper backgroundImage={background}>
+            <FilterHeader>
+                <Title title={`Filter by site type`} hasArrow={true} />
+            </FilterHeader>
+
+            {filters.map((item, i) => (
+                <ListTileCheckbox
+                    key={i}
+                    label={item.label}
+                    trailingIcon={item.icon}
+                    onClick={() => setBackground(item.background)} />
+            ))}
         </ScreenWrapper>
     );
 };
