40e663fe7fda5ab79600f7c23f2b1468626aca86
Robert Ngo <robert.ngo@evolvingweb.ca>
2021-03-09 23:19:45 -0500

    refs #26468: add placeholder for screens.

---
+++ src/screens/filters/filterNearMe.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ScreenWrapper from '../../components/screenWrapper';
+import {Text} from 'react-native';
+
+const FilterByNearMeScreen = () => {
+    return (
+        <ScreenWrapper>
+            <Text>FilterByNearMeScreen</Text>
+        </ScreenWrapper>
+    );
+};
+
+export default FilterByNearMeScreen;
