9659de4ee37038288f56bf65a08599cd9fd37d0b
Robert Ngo <robert.ngo@evolvingweb.ca>
2021-03-10 23:35:41 -0500

    refs #26481: add current conditions content.

--- src/screens/helpfulInfo/currentConditions.js
+++ src/screens/helpfulInfo/currentConditions.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {View, Text, Image, Dimensions, SafeAreaView} from 'react-native';
 
-import {Body1, H2} from '../../theme/typings';
+import {Body1, H2, H3} from '../../theme/typings';
 
 import ScreenParallaxWrapper from '../../components/screenParallaxWrapper';
 import CurrentConditionStyles from '../../stacks/more/screens/current-conditions/current-conditions.styles';
@@ -16,12 +16,21 @@ const CurrentConditionsScreen = () => {
                                leadIcon={iconCurrentConditions}
                                title={'Current Conditions'}>
             <>
-                <H2 black>Campground closures and warning</H2>
+                <H2 black style={{ marginBottom: 32 }}>Campground closures and warning</H2>
                 <Body1 regular black>
                     Information on Government of Yukon’s campgrounds, including campground maps,
-                    closures and public warnings, visit.
+                    closures and public warnings, visit yukon.ca.
                 </Body1>
-                <View style={CurrentConditionStyles.separator}/>
+
+                <H3 black style={{ marginBottom: 12, marginTop: 24}}>Highway conditions</H3>
+                <Body1 regular black>
+                    Yukon’s highway reports on current road conditions, warnings and closures, visit 511yukon.ca
+                </Body1>
+
+                <H3 black style={{ marginBottom: 12, marginTop: 24}}>Fire reports</H3>
+                <Body1 regular black>
+                    Current information on Yukon’s fire investigation reports and public safety notices, visit yukon.ca
+                </Body1>
 
             </>
 
