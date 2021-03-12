4eb50acc693d3ca0530977396fb20ed60fb48fe2
Robert Ngo <robert.ngo@evolvingweb.ca>
2021-03-11 23:57:56 -0500

    refs #26481: add shadow on top of ParallaxWrapper
    Former-commit-id: 9b37e976080f0c09551930609845a2bcb074f3b0

--- src/components/screenParallaxWrapper/index.js
+++ src/components/screenParallaxWrapper/index.js
@@ -10,6 +10,7 @@ const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
 const auroraOrange = require('./images/aurora-orange.png');
+const shadow = require('./images/shadow.png');
 
 const ScreenParallaxWrapper = (props) => {
 
@@ -26,6 +27,11 @@ const ScreenParallaxWrapper = (props) => {
             renderForeground={() => (
 
                 <View style={{flex: 1, justifyContent: 'space-between'}}>
+                    <Image source={shadow} style={{
+                        position: 'absolute',
+                        bottom: 0, left: 0, width: windowWidth,
+                        resizeMode: 'cover',
+                    }}/>
                     <SafeAreaView>
                         <View style={{ margin: 18 }}>
                             <BackButton />
