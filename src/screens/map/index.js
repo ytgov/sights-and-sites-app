f2c15790eea04f4941e8f58413952eaabd12e643
Robert Ngo <robert.ngo@evolvingweb.ca>
2021-03-12 08:53:29 -0500

    refs #26481: WIP
    Former-commit-id: bd2ad1dd8683adb63f5d30b2ea5f9bd1765fb916

--- src/screens/map/index.js
+++ src/screens/map/index.js
@@ -1,13 +1,42 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Dimensions, Image, ScrollView, Text, View} from 'react-native';
 import ScreenWrapper from '../../components/screenWrapper';
 import HeaderNav, {HeaderNavType} from '../../components/headerNav';
 
+const windowWidth = Dimensions.get('window').width;
+
 const MapScreen = () => {
+    const ratio = 750 / 350;
+
+    const getTest = (k) => {
+        const width = windowWidth * k
+        const height = parseInt((width * ratio).toFixed(0))
+        const url = `https://picsum.photos/${width}/${height}`
+        return (
+            <View style={{ marginBottom: 50 }}>
+                <View style={{
+                    top: 16, left: 16, height: 50,
+                    position: 'absolute',
+                    zIndex: 10,
+                    backgroundColor: 'black',
+                    padding: 8
+                }}>
+                    <Text style={{ color: 'white'}}>{width} x {height}</Text>
+                    <Text style={{ color: 'white'}}>{url}</Text>
+                </View>
+                <Image source={{uri: url}}
+                       style={{
+                           width: width,
+                           height: height, resizeMode: 'contain' }} />
+            </View>
+        )
+    }
+
     return (
-        <ScreenWrapper>
-            <Text>MapScreen</Text>
-        </ScreenWrapper>
+        <ScrollView>
+            {getTest(1)}
+            {getTest(2)}
+        </ScrollView>
     );
 };
 
