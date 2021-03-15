f4ed2ccba95c64552c2de61236aaef62ded49d0d
Robert Ngo <robert.ngo@evolvingweb.ca>
2021-03-15 19:56:09 -0400

    refs #26600: add show, hide header reducer, add header animation.

--- src/components/headerNav/index.js
+++ src/components/headerNav/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
 import PropTypes from 'prop-types';
-import {View} from 'react-native';
+import {View, Animated} from 'react-native';
 import {useTranslation} from 'react-i18next';
 
 import {withNavigation} from 'react-navigation';
@@ -8,6 +8,7 @@ import routes from '../../navigation/routes';
 
 import indexStyles from './index.styles';
 import HeaderNavItem from './headerNavItem';
+import {connect} from 'react-redux';
 
 const filtersIcon = require('./images/filters.png');
 const listIcon = require('./images/list.png');
@@ -19,12 +20,46 @@ export const HeaderNavType = {
     MAP: 'map'
 }
 
-const HeaderNav = ({scene, activeItem, navigation}) => {
+const HeaderNav = ({scene, activeItem, navigation, headerVisible}) => {
     const {t} = useTranslation();
+    const slideAnim = useRef(new Animated.Value(1)).current
+
+    const animateComponent = (toValue, duration) => {
+        Animated.timing(
+            slideAnim,
+            {
+                toValue,
+                duration
+            }
+        ).start()
+    }
+
+    if (headerVisible) {
+        animateComponent(1, 100)
+    } else {
+        animateComponent(0, 200)
+    }
+
+    const transformValue = {
+        transform: [
+            {
+                translateY: slideAnim.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [-110, 0]
+                })
+            }
+        ]
+    }
+
     const { options } = scene.descriptor;
 
     return (
-        <View style={[options.headerStyle, indexStyles.wrapper]}>
+        <Animated.View style={{
+            ...options.headerStyle,
+            ...indexStyles.wrapper,
+            ...transformValue,
+            backgroundColor: 'green'
+        }}>
             <HeaderNavItem icon={filtersIcon}
                            label={t('navigation.header.filters')}
                            isActive={activeItem === HeaderNavType.FILTERS}
@@ -37,7 +72,7 @@ const HeaderNav = ({scene, activeItem, navigation}) => {
                            label={t('navigation.header.map')}
                            isActive={activeItem === HeaderNavType.MAP}
                            onPress={() => navigation.navigate(routes.SCREEN_MAP)} />
-        </View>
+        </Animated.View>
     );
 };
 
@@ -49,6 +84,12 @@ HeaderNav.propTypes = {
     ])
 }
 
-export default withNavigation(HeaderNav);
+const mapStateToProps = (state) => {
+    return {
+        headerVisible: state.coreStore.headerVisible
+    };
+};
+
+export default connect(mapStateToProps, null)(withNavigation(HeaderNav));
 
 
