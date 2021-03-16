d5c0b098fd2b276df673c84cbdfc156744e462f9
Robert Ngo <robert.ngo@evolvingweb.ca>
2021-03-16 15:05:52 -0400

    refs #26469: create model SiteType.
    Former-commit-id: f05e84b010960257e483793cef55fcc27a460bc0

---
+++ src/models/SiteType.js
@@ -0,0 +1,8 @@
+export default class SiteType {
+    constructor(id = '', name = '', icon = null, background = null) {
+        this.id = id;
+        this.name = name;
+        this.icon = icon;
+        this.background = background;
+    }
+}
