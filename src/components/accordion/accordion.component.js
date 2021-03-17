613e58584700ce58ba450194ed3149772d11d1da
Diego Castro <diego.castro@evolvingweb.ca>
2021-03-17 12:11:10 -0500

    refs #26481: Refactor static pages. - App Information page.
    Former-commit-id: 1e4c022d6b30c6481dfa420ef19e6a3d885d2645

---
+++ src/components/accordion/accordion.component.js
@@ -0,0 +1,128 @@
+import React from 'react';
+import {Accordion} from 'native-base';
+import {Image, Linking, TouchableOpacity, View, Text} from 'react-native';
+import AccordionType from '../../types/accordion.type';
+import {Body1, H3} from '../../theme/theme';
+import COMMON from '../../theme/common';
+
+import {Body2} from '../../theme/typings';
+
+const accordionClosedIcon = require('../../../../../assets/stacks/more/accordion-closed-icon.png');
+const accordionOpenedIcon = require('../../../../../assets/stacks/more/accordion-opened-icon.png');
+
+const AccordionCustom = props => {
+    const {data} = props;
+    return (
+        <Accordion
+            animation
+            style={{
+                backgroundColor: 'transparent',
+                borderTopWidth: 0,
+                borderLeftWidth: 0,
+                borderRightWidth: 0,
+                borderBottomWidth: 0,
+            }}
+            renderHeader={(item, expanded) => <View style={{
+                flexDirection: 'row',
+                paddingTop: 15,
+                paddingBottom: 15,
+                paddingLeft: 5,
+                paddingRight: 5,
+                justifyContent: 'flex-start',
+                alignItems: 'center',
+                borderBottomColor: '#CBCBCB',
+                borderBottomWidth: 1
+            }}>
+                <Image source={expanded ? accordionOpenedIcon : accordionClosedIcon} resizeMode='contain'
+                       style={{width: 20, height: 20, marginRight: 17}}/>
+                <H3 style={{width: '90%'}} black>{item.title}</H3>
+            </View>}
+            renderContent={item => <View style={{
+                paddingTop: 15,
+                paddingBottom: 15,
+                paddingLeft: 5,
+                paddingRight: 5,
+                borderBottomColor: '#CBCBCB',
+                borderBottomWidth: 1
+            }}>
+
+                <Body1 regular black>
+                    {item.content_italicized ? (<Text style={{fontFamily: 'montserrat-italic'}}>{item.content_italicized}</Text>) : null}
+
+                    {item.content ? item.content + '\n' : null}
+                </Body1>
+                {item.url ?
+                    <View style={COMMON.linkBox}>
+                        <TouchableOpacity onPress={() => {
+                            Linking.openURL(item.url)
+                        }}>
+                            <View style={COMMON.link}><Body1
+                                black>{item.url_text ? item.url_text : 'More safe travel tips?'}</Body1></View>
+                        </TouchableOpacity>
+                    </View>
+                    : null
+                }
+                {item.contacts ?
+                    item.contacts.map((m, i) => <Body1 key={i} bold black>{m.name}: {m.contact}</Body1>)
+                    : null
+                }
+
+                {
+                    item.QA ?
+                        item.QA.map((m, i) => {
+                            return (
+                                <View key={i}>
+                                    <Body1 bold black> {m.q + '\n'}</Body1>
+                                    <Body1 regular black>{m.a + '\n'}</Body1>
+                                    {
+                                        m.list ? m.list.map((item, index) => {
+                                            return (
+                                                <View style={{paddingLeft: 10}} key={`list-${index}`}>
+                                                    {item.q ? <Body2 bold black> {item.q + '\n'}</Body2> : null}
+                                                    {item.a ? <Body2 regular black>{item.a + '\n'}</Body2> : null}
+                                                    {
+                                                        item.items ? item.items.map((text, aa) => (
+                                                            <View style={{paddingLeft: 10, flexDirection: 'row'}}
+                                                                  key={`list-${index}-${aa}`}>
+                                                                <Body2 bold black style={{
+                                                                    paddingRight: 5,
+                                                                    paddingTop: 2.5
+                                                                }}>*</Body2>
+                                                                <Body2 regular black>{text + '\n'}</Body2>
+                                                            </View>
+                                                        )) : null
+                                                    }
+                                                </View>
+                                            )
+                                        }) : null
+                                    }
+                                    {
+                                        m.links ? m.links.map((link, index) => (
+                                            <View key={`link-${index}`} style={COMMON.linkBox}>
+                                                <TouchableOpacity onPress={() => {
+                                                    Linking.openURL(link.url)
+                                                }}>
+                                                    <View style={COMMON.link}><Body2 black>{link.title}</Body2></View>
+                                                </TouchableOpacity>
+                                            </View>
+                                        )) : null
+                                    }
+                                </View>
+                            )
+                        })
+                        : null
+                }
+
+
+            </View>}
+            dataArray={data} expanded={[0]}/>
+    )
+}
+
+AccordionCustom.propTypes = {
+    data: AccordionType
+}
+
+AccordionCustom.defaultProps = [];
+
+export default AccordionCustom;
--- src/screens/helpfulInfo/appInformation.js
+++ src/screens/helpfulInfo/appInformation.js
@@ -1,23 +1,111 @@
 import React from 'react';
-import {Body1, H2, H3} from '../../theme/typings';
-
+import AccordionCustom from '../../components/accordion/accordion.component';
+import { useTranslation } from 'react-i18next';
 import ScreenParallaxWrapper from '../../components/screenParallaxWrapper';
+import HTMLElement from '../../components/htmlElement';
+import i18n from '../../locale/locale';
+import Section from '../siteDetails/section';
 const bgAppInformation = require('./images/bg-app-information.png');
 const iconAppInformation = require('./images/icon-app-information.png');
 
+const accordionData = [
+    {
+        title: i18n.t('appInformation.locationServices.title'),
+        content: '',
+        QA: [
+            {
+                q: i18n.t('appInformation.locationServices.questions.q1'),
+                a: i18n.t('appInformation.locationServices.questions.a1'),
+            },
+            {
+                q: i18n.t('appInformation.locationServices.questions.q2'),
+                a: i18n.t('appInformation.locationServices.questions.a2'),
+            }
+        ]
+
+    },
+    {
+        title: i18n.t('appInformation.mobileData.title'),
+        content: i18n.t('appInformation.mobileData.content'),
+        QA: [
+            {
+                q: i18n.t('appInformation.mobileData.questions.q1'),
+                a: i18n.t('appInformation.mobileData.questions.a1'),
+            },
+            {
+                q: i18n.t('appInformation.mobileData.questions.q2'),
+                a: i18n.t('appInformation.mobileData.questions.a2'),
+            }
+        ]
+    },
+    {
+        title: i18n.t('appInformation.privacyPolicy.title'),
+        content: i18n.t('appInformation.privacyPolicy.content'),
+    },
+    {
+        title: i18n.t('appInformation.termsConditions.title'),
+        content: '',
+        QA: [
+            {
+                q: i18n.t('appInformation.termsConditions.questions.q1'),
+                a: i18n.t('appInformation.termsConditions.questions.a1'),
+            },
+            {
+                q: i18n.t('appInformation.termsConditions.questions.q2'),
+                a: i18n.t('appInformation.termsConditions.questions.a2'),
+            },
+            {
+                q: i18n.t('appInformation.termsConditions.questions.q3'),
+                a: i18n.t('appInformation.termsConditions.questions.a3'),
+                list: [
+                    {
+                        q: i18n.t('appInformation.termsConditions.questions.q3List.q1'),
+                        a: i18n.t('appInformation.termsConditions.questions.q3List.a1')
+                    },
+                    {
+                        q: i18n.t('appInformation.termsConditions.questions.q3List.q2'),
+                        a: i18n.t('appInformation.termsConditions.questions.q3List.a2'),
+                    },
+                    {
+                        a: i18n.t('appInformation.termsConditions.questions.q3List.a3'),
+                        items: [
+                            i18n.t('appInformation.termsConditions.questions.q3List.q3items.first'),
+                            i18n.t('appInformation.termsConditions.questions.q3List.q3items.second'),
+                        ]
+                    }
+                ]
+            },
+            {
+                q: i18n.t('appInformation.termsConditions.questions.q4'),
+                a: i18n.t('appInformation.termsConditions.questions.a4'),
+                links: [
+                    {
+                        url: i18n.t('appInformation.termsConditions.questions.q4Links.url1'),
+                        title: i18n.t('appInformation.termsConditions.questions.q4Links.title1'),
+                    },
+                    {
+                        url: i18n.t('appInformation.termsConditions.questions.q4Links.url2'),
+                        title: i18n.t('appInformation.termsConditions.questions.q4Links.title2'),
+                    }
+                ]
+            }
+        ],
+    }
+];
+
 const AppInformationScreen = () => {
+    const { t } = useTranslation();
+
     return (
         <ScreenParallaxWrapper backgroundImage={bgAppInformation}
                                leadIcon={iconAppInformation}
                                title={'App information'}>
-            <>
-                <H2 black style={{ marginBottom: 32 }}>Let the Yukon Road Trip app be your guide</H2>
-                <Body1 regular black>
-                    The Yukon Road Trip app is your guide to Government of Yukon’s road accessible
-                    campgrounds, wildlife viewing, cultural and historical points of interest.
-                    Connect with the natural and cultural history that surrounds you.
-                </Body1>
-            </>
+            <Section title={t('appInformation.title')}>
+                <HTMLElement html={t('appInformation.intro')} />
+            </Section>
+            <Section>
+                <AccordionCustom data={accordionData}/>
+            </Section>
 
         </ScreenParallaxWrapper>
     );
