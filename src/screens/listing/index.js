c7c0962086e9116d4d58fbb16fb53486bd5570b4
Robert Ngo <robert.ngo@evolvingweb.ca>
2021-03-17 23:43:49 -0400

    refs #26596: implement load more on demand

--- src/screens/listing/index.js
+++ src/screens/listing/index.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import {ScrollView, FlatList, TouchableOpacity, Text} from 'react-native';
-
+import React, {useState} from 'react';
+import {FlatList, TouchableOpacity, Dimensions} from 'react-native';
 
 import {NavigationEvents} from 'react-navigation';
 import {connect} from 'react-redux';
@@ -11,18 +10,37 @@ import HeaderNav, {HeaderNavType} from '~components/headerNav';
 import SiteCard from '~components/siteCard';
 import NoResult from '~components/noResult';
 
+const windowHeight = Dimensions.get('window').height
+
+const ITEMS_PER_PAGE = 10
+
 const ListingScreen = (props) => {
     const {navigation, dispatchShowHeader, listingFiltered} = props
+    const [items, setItems] = useState(listingFiltered.slice(0, ITEMS_PER_PAGE))
+    const [page, setPage] = useState(1)
+    const maxPages = Math.ceil(listingFiltered.length / ITEMS_PER_PAGE)
 
-    const data = listingFiltered.slice(0, 10)
+    const loadMore = () => {
+        const newPage = page + 1
+        setPage(newPage)
+        if (newPage < maxPages) {
+            const newBatch = listingFiltered.slice(0, (page + 1) * ITEMS_PER_PAGE)
+            setItems(newBatch)
+        } else if (newPage === maxPages) {
+            setItems(listingFiltered)
+        }
+    }
 
     return (
-        <ScrollView scrollEventThrottle={16}>
+        <>
             <NavigationEvents
                 onWillFocus={payload => dispatchShowHeader()} />
 
             <FlatList
-                data={data}
+                style={{height: windowHeight}}
+                data={items}
+                initialNumToRender={3}
+                scrollEventThrottle={16}
                 renderItem={({item, i}) =>
                     <TouchableOpacity activeOpacity={0.8}
                                       onPress={() => navigation.navigate(routes.SCREEN_SITE_DETAILS, {item})}>
@@ -30,8 +48,12 @@ const ListingScreen = (props) => {
                     </TouchableOpacity>}
                 keyExtractor={(item) => item.site_id.toString()}
                 ListEmptyComponent={<NoResult />}
+                // On End Reached (Takes a function)
+                onEndReached={loadMore}
+                // Load more when reaching the last item
+                onEndReachedThreshold={0.5}
             />
-        </ScrollView>
+        </>
     );
 };
 
