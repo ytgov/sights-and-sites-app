0ffe17d5adec2115736b14b249d8db67720cc02c
Robert Ngo <robert.ngo@evolvingweb.ca>
2021-03-30 22:11:28 -0400

    refs #26472: add user location and distance actions and reducers.
    Former-commit-id: b90d6d4cb82b636bfc19f45ef58339d58f01f6da

--- src/store/reducers/core.js
+++ src/store/reducers/core.js
@@ -9,17 +9,19 @@ import {
     TOGGLE_SEARCH,
     SHOW_SEARCH,
     HIDE_SEARCH,
+    GET_USER_LOCATION_SUCCESS,
+    SET_SITE_DISTANCE,
     SET_CURRENT_SCREEN_NAME,
 } from '../types';
 
 const initialState = {
     hasUserSelectedLocale: false,
     hasUserPassedOnboarding: false,
-    canGrabLocation: true,
-    location: null,
+    userLocation: null,
     networkAvailable: true,
     headerVisible: true,
-    searchVisible: false
+    searchVisible: false,
+    distances: {}
 }
 
 export default function coreReducer(state = initialState, action) {
@@ -34,34 +36,6 @@ export default function coreReducer(state = initialState, action) {
             return {
                 ...state,
                 hasUserPassedOnboarding: true
-            }
-        }
-        case UPDATE_LOCATION: {
-            return {
-                ...state,
-                canGrabLocation: true,
-                // location: { // Just for testing canada
-                //     latitude: 46.333389,
-                //     longitude: -75.58429,
-                // },
-                // location: { // Just for testing tunisia
-                //     latitude: 43.7452,
-                //     longitude: 10.7613,
-                // },
-                location: action.payload
-            }
-        }
-        case RESET_LOCATION: {
-            return {
-                ...state,
-                location: null,
-                canGrabLocation: false
-            }
-        }
-        case SET_NETWORK_STATUS: {
-            return {
-                ...state,
-                networkAvailable: action.payload
             }
         }
 
@@ -88,6 +62,22 @@ export default function coreReducer(state = initialState, action) {
                 ...state,
                 currentScreenName: action.payload
             }
+
+        case GET_USER_LOCATION_SUCCESS:
+            return {
+                ...state,
+                userLocation: action.payload.coordinates
+            }
+
+        case SET_SITE_DISTANCE:
+            return {
+                ...state,
+                distances: {
+                    ...state.distances,
+                    ...{[action.payload.site_id]: action.payload}
+                }
+            }
+
         default:
             return state;
     }
