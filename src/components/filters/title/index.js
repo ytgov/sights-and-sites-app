c53a444272daa6d1596394bd7c860e77fdb9f340
Robert Ngo <robert.ngo@evolvingweb.ca>
2021-03-31 10:55:45 -0400

    refs #26469: enlarge back button clickable area.
    Former-commit-id: dac2b385ce4230b660a38541d42eaf9eeba1ba5d

--- src/components/filters/title/index.js
+++ src/components/filters/title/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {View, Image, TouchableWithoutFeedback} from 'react-native';
+import {View, Image, TouchableOpacity} from 'react-native';
 import {withNavigation} from 'react-navigation';
 
-import {H2} from '../../../theme/typings';
+import {H2} from '~theme/typings';
 import styles from './styles';
 
 const arrow = require('./images/arrow-back.png');
 
 const Title = ({navigation, title, hasArrow}) => {
     return <View style={styles.wrapper}>
-        {hasArrow && <TouchableWithoutFeedback onPress={() => navigation.pop()}>
-            <Image source={arrow} style={styles.arrow} />
-        </TouchableWithoutFeedback>}
+        {hasArrow &&
+            <TouchableOpacity
+                    activeOpacity={0.8}
+                    style={styles.arrowWrapper}
+                    onPress={() => navigation.pop()}>
+                <Image source={arrow} style={styles.arrow} />
+            </TouchableOpacity>}
         <H2>{title}</H2>
     </View>
 };
