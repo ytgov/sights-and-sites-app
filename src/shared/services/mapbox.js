3cad536c9674cd4c3fefb3b3380fcdbbe8e9c6cf
Robert Ngo <robert.ngo@evolvingweb.ca>
2021-03-30 22:10:20 -0400

    refs #26472: add service Mapbox.
    Former-commit-id: 3ea45175aa3bc24f16423302e1c338ff6b327697

---
+++ src/shared/services/mapbox.js
@@ -0,0 +1,44 @@
+import MapboxClient from '@mapbox/mapbox-sdk';
+import MapboxDirectionClient from '@mapbox/mapbox-sdk/services/directions';
+import {APP_CONFIG} from '../../config';
+
+const accessToken = APP_CONFIG.map_box;
+const mapboxClient = new MapboxClient({accessToken});
+const mapboxDirectionClient = new MapboxDirectionClient(mapboxClient);
+
+export const getDrivingDistance = async (source, destination) => {
+    let result = {};
+    try {
+        let response = await mapboxDirectionClient.getDirections({
+            profile: 'driving-traffic',
+            waypoints: [
+                {
+                    coordinates: [source.longitude, source.latitude]
+                },
+                {
+                    coordinates: [destination.longitude, destination.latitude]
+                },
+            ],
+            geometries: 'geojson'
+        }).send();
+        if (response !== null) {
+            let route = response.body.routes[0];
+            result =  {
+                status: 200,
+                fetched: true,
+                source: source,
+                distance: route.distance,
+                duration: route.duration,
+                coordinates: route.geometry.coordinates
+            }
+        }
+
+    } catch (e) {
+        result = {
+            message: e.message
+        }
+        console.info('======> Got ERROR : ', e.message);
+    }
+
+    return result;
+}
