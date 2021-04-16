eeebfacd767db702e735d4240b035c85813f0966
Robert Ngo <robert.ngo@evolvingweb.ca>
2021-04-16 16:10:23 -0400

    refs #26088: update french translations.
    Former-commit-id: 5871d4dc751ec20ea1e9857b654e39e31695bc16

--- src/locale/fr.js
+++ src/locale/fr.js
@@ -2,14 +2,15 @@ export default {
     'translation': {
         appInformation: {
             'title': "Renseignements sur l'application",
-            'intro': `Cette appli est votre guide sur les terrains de camping, la vie sauvage ainsi que les lieux d’intérêt culturel et historique qui sont accessibles par le réseau routier du Yukon. Explorez les sites par route ou par région ou bien recherchez un site à proximité. Découvrez l’histoire naturelle et culturelle qui vous entoure.`,
+            'intro': `Cette appli est votre guide pour les terrains de camping, la vie sauvage, et les lieux d’intérêt culturel et historique qui sont accessibles par le réseau routier du Yukon. Découvrez l’histoire naturelle et culturelle qui vous entoure.`,
             locationServices: {
                 title: "Géolocalisation",
                 questions: {
                     q1: `Pourquoi l’application cherche-t-elle à me localiser?`,
-                    a1: `L’application utilise la localisation fournie par votre appareil pour vous donner des résultats utiles, comme des lieux situés à proximité.\n Cette application ne conserve pas la localisation en mémoire. La localisation n’est utilisée que dans votre appareil. Vous pouvez activer la localisation dans l’application ou sur votre appareil.`,
-                    q2: `Je ne souhaite pas activer l’option de localisation.`,
-                    a2: `Aucun problème! Vous pouvez quand même rechercher et filtrer des lieux. Cependant, vous ne pourrez pas naviguer jusqu’à ces lieux ou voir les lieux situés à proximité dans l’application. Si votre appareil est connecté à Internet, il utilisera les données mobiles pour mettre l’application à jour, au besoin.`,
+                    a1: `L’application utilise la localisation fournie par votre appareil pour vous donner des résultats utiles, comme les lieux situés à proximité.`
+                        + `L'application ne conserve pas la localisation en mémoire; elle n’est utilisée que dans votre appareil. Vous pouvez activer la localisation dans l’application ou sur votre appareil.`,
+                    q2: `Je ne souhaite pas activer la géolocalisation.`,
+                    a2: `Pas de problème! Vous pouvez quand même faire une recherche et utiliser les filtres. Cependant, vous ne pourrez pas naviguer jusqu’à ces lieux ni voir ceux qui sont situés à proximité.`,
                 },
             },
             mobileData: {
@@ -17,35 +18,37 @@ export default {
                 content: 'Si votre appareil est connecté à Internet, il utilisera les données mobiles pour mettre l’application à jour, au besoin.',
                 questions: {
                     q1: 'Puis-je utiliser l\'application hors ligne?',
-                    a1: `L’application fonctionne entièrement hors ligne. Les cartes se téléchargent sur votre appareil si vous autorisez l’application à le faire.`,
-                    q2: 'Quand les données de l’application sont-elles mises à jour?',
-                    a2: 'L’application est mise à jour périodiquement lorsque de nouvelles données sont disponibles, par exemple :\n\n' +
+                    a1: `Absolument. Vous pouvez télécharger les cartes sur votre appareil si vous y autorisez l’application.`,
+                    q2: 'À quelle fréquence l’application est-elle mise à jour?',
+                    a2: 'L’application est mise à jour périodiquement, lorsque de nouvelles données sont disponibles, par exemple :\n\n' +
                         '\u2022 Nouveaux lieux\n' +
                         '\u2022 Services relatifs à des lieux existants\n' +
                         '\u2022 Itinéraires\n' +
-                        '\u2022 Détails sur les cartes\n\n',
+                        '\u2022 Données cartographiques\n\n',
                 },
             },
             privacyPolicy: {
                 title: 'Politique de confidentialité',
-                content: `Le gouvernement du Yukon s'engage à protéger votre vie privée. La collecte, l'utilisation, la divulgation, la conservation et l'élimination des informations recueillies via les propriétés en ligne du gouvernement du Yukon, telles que nos sites Web, se font conformément à la Loi sur l'accès à l'information et la protection de la vie privée et à la Loi sur les archives.`,
+                content: `Le gouvernement du Yukon s'engage à protéger vos renseignements personnels. La collecte, l'utilisation, la communication, la conservation et l'élimination des renseignements recueillis par les propriétés en ligne du gouvernement du Yukon, comme ses sites Web, se font conformément à la Loi sur l'accès à l'information et la protection de la vie privée et à la Loi sur les archives.\n\n`
+                    + `Cet énoncé vise à vous informer des renseignements personnels qui peuvent être recueillis quand vous interagissez avec le gouvernement du Yukon en ligne.`
+                ,
             },
             termsConditions: {
-                title: 'Termes et conditions',
+                title: 'Conditions',
                 questions: {
-                    q1: 'Avis de non responsabilité',
-                    a1: 'Ces renseignements sont distribués « tels quels » et ne présentent aucune garantie exprimée ou implicite. L’information peut être utilisée à la condition qu’il soit formellement entendu que ni le gouvernement, ni ses ministres, employés ou agents ne seront responsables des pertes ou des dommages de quelque nature que ce soit qui pourraient résulter de l’utilisation des renseignements fournis dans cette application ou sur les sites externes pour lesquels elle offre des liens.',
+                    q1: 'Avis de non-responsabilité',
+                    a1: 'L\'information est distribuée telle quelle et ne présente aucune garantie exprimée ni implicite. L’information peut être utilisée à la condition qu’il soit formellement entendu que ni le gouvernement, ni ses ministres, employés ou agents ne seront responsables des pertes ou des dommages de quelque nature que ce soit qui pourraient résulter de l’utilisation de l\'information contenue dans l\'application ou sur les sites externes vers lesquels mènent les liens.',
                     q2: 'Avis de confidentialité',
-                    a2: `Le gouvernement du Yukon s’engage à protéger vos renseignements personnels. La collecte, l’utilisation, la divulgation, la conservation et l’élimination de l’information recueillie à partir des ressources en ligne du gouvernement du Yukon telles que les sites Web et les applications mobiles sont faits en conformité avec la Loi sur l’accès à l’information et la protection des renseignements personnels et la Loi sur les archives.\n\n Le présent avis de confidentialité a pour objet de vous faire savoir quels renseignements personnels pourraient être recueillis lorsque vous utilisez cette application.`,
+                    a2: `Le gouvernement du Yukon s’engage à protéger vos renseignements personnels. La collecte, l’utilisation, la communication, la conservation et l’élimination des renseignements recueillis à partir des ressources en ligne du gouvernement du Yukon, comme les sites Web et les applications mobiles, sont faites en conformité avec la Loi sur l’accès à l’information et la protection des renseignements personnels et la Loi sur les archives.`,
                     q3: 'Collecte de renseignements personnels',
-                    a3: 'La collecte de renseignements personnels recueillis à partir des sites Web du gouvernement est effectuée conformément au paragraphe 29c) de la Loi sur l’accès à l’information et la protection de la vie privée aux fins indiquées ci-après: \n',
+                    a3: 'La collecte de renseignements personnels à partir des sites Web du gouvernement est effectuée conformément au paragraphe 29c) de la Loi sur l’accès à l’information et la protection de la vie privée aux fins indiquées ci-dessous. \n',
                     q3List: {
-                        q1: '1. Communiquer avec le ministère du Tourisme et de la Culture, gouvernement du Yukon',
-                        a1: 'Si vous choisissez d’envoyer par courriel une demande de renseignements ou des commentaires généraux sur l’application mobile, vos renseignements personnels peuvent être utilisés pour répondre à votre courriel.\n' +
+                        q1: '1. Communiquer avec le ministère du Tourisme et de la Culture du gouvernement du Yukon',
+                        a1: 'Si vous choisissez d’envoyer une demande de renseignements ou des commentaires généraux par courriel à partir de l’application mobile, vos renseignements personnels pourraient être utilisés pour vous répondre.\n' +
                             'Les courriels et autres méthodes électroniques utilisées pour communiquer avec le gouvernement du Yukon ne sont pas protégés, à moins d’indication contraire, clairement énoncée. Il est donc recommandé de ne pas transmettre de renseignements personnels de nature délicate par ces moyens.',
                         q2: '2. Fichier de journalisation',
-                        a2: 'Nous recueillons les informations que votre appareil envoie lorsque vous utilisez notre application mobile (« fichier de journalisation »).',
-                        a3: 'Le fichier de journalisation contient des informations telles que :',
+                        a2: 'Nous recueillons les informations transmises par votre appareil quand vous utilisez l\'application.',
+                        a3: 'Le fichier de journalisation contient des informations comme :',
                         q3items: {
                             first: 'l’adresse de protocole Internet (adresse IP) de votre ordinateur,',
                             second: 'la date et l’heure de la demande.',
@@ -107,36 +110,36 @@ export default {
                 title: 'Type de site'
             },
             sectionInfo: {
-                title: 'Information',
-                warning: 'Warning:'
+                title: 'Informations',
+                warning: 'Attention :'
             },
             sectionDescription: {
                 title: 'Description'
             },
             sectionDirections: {
-                title: 'Directions',
-                highway: 'Highway',
-                highwayKm: 'Highway kilometre',
-                secondaryRoad: 'Secondary road',
-                secondaryRoadKm: 'Secondary road kilometre',
+                title: 'Itinéraire',
+                highway: 'Route',
+                highwayKm: 'Borne kilométrique',
+                secondaryRoad: 'Route secondaire',
+                secondaryRoadKm: 'Borne kilométrique',
                 GPS: 'GPS coordinates',
-                siteDirections: 'Site directions'
+                siteDirections: 'Itinéraire'
             },
             sectionHelpfulLinks: {
-                title: 'Helpful links',
-                getCampingPermit: 'Get a camping permit',
-                rulesSafety: 'Rules and safety',
-                guideToCamping: 'Guide to camping in Yukon',
-                map: 'Map (campgrounds & recreation sites)'
+                title: 'Liens utiles',
+                getCampingPermit: 'Permis de camping',
+                rulesSafety: 'Règles et mesures de sécurité',
+                guideToCamping: 'Guide du camping au Yukon',
+                map: 'Carte (terrains de camping et aires de loisirs)'
             },
             sectionBooking: {
-                title: 'Are you interested in booking this beautiful site?',
-                text: 'To book a site, please visit the Yukon Road Trip page on Yukon.ca to get more detailed information on the next steps needed to plan your trip. Thank you for your interest!',
+                title: 'Vous voulez réserver ce magnifique emplacement?',
+                text: 'Pour faire une réservation, allez à la page Virée en voiture au Yukon sur yukon.ca. Merci!',
                 linkLabel: 'Yukon.ca/en/road-trip-app',
-                extra: '<p style="color: white">For questions and support regarding this app, please feel free to email ' +
-                    '<a href="mailto:heritage.planning@gov.yk.ca">heritage.planning@gov.yk.ca</a> or ' +
-                    'phone <a href="tel:867-667-5386">867-667-5386</a>, or toll free in Yukon <a href="tel:1-800-661-0408">1-800-661-0408</a>, ' +
-                    'extension 5386.</p>'
+                extra: '<p style="color: white">Besoin d\'aide? Des questions? Écrivez à ' +
+                    '<a href="mailto:heritage.planning@gov.yk.ca">heritage.planning@gov.yk.ca</a> ou ' +
+                    'appelez au <a href="tel:867-667-5386">867-667-5386</a>, ou au <a href="tel:1-800-661-0408">1-800-661-0408</a>, ' +
+                    'poste 5386 (sans frais au Yukon).</p>'
             },
             sectionSupport: {
                 title: 'Support the App!',
@@ -156,7 +159,11 @@ export default {
             highways: 'Route',
             highwayTitle: 'Filtrer par route',
             nearMe: 'À proximité',
-            myFavorites: 'Coups de cœur'
+            myFavorites: 'Coups de cœur',
+            notifications: {
+                reset: 'Filtres réinitialisés',
+                applied: 'Filtres appliqués'
+            }
         },
         filterRegions: {
             campbell: 'Campbell',
@@ -217,12 +224,12 @@ export default {
                 title: 'Conseils pour observer la faune sans danger près de la route',
                 stop: '<strong>ARRÊTEZ-VOUS</strong> seulement si vous pouvez le faire en toute sécurité, c’est-à-dire après avoir vérifié la circulation et vous être assuré que votre véhicule est bien visible.',
                 look: '<strong>OBSERVEZ</strong> sans sortir de votre véhicule. Utilisez des jumelles ou un objectif à focale variable pour voir de plus près.',
-                leave: '<strong>REPARTEZ</strong> dans un délai d’une minute pour garantir la sécurité des animaux. Les animaux qui s\'habituent aux humains risquent de se faire tuer.',
+                leave: '<strong>REPARTEZ</strong> dans un délai d’une minute pour garantir la sécurité des animaux. Les animaux qui s’habituent aux humains risquent de se faire tuer.',
             },
             data: {
                 title1: 'Préservation du patrimoine historique',
-                content_italicized1: 'La Loi sur le patrimoine historique',
-                content1: ' garantit la préservation du patrimoine historique du Yukon. Ne déplacez jamais un artéfact d’un lieu archéologique ou historique. Pour signaler une découverte, contactez ',
+                content_italicized1: 'Le patrimoine historique du Yukon',
+                content1: ' est protégé par la Loi sur le patrimoine historique. Ne retirez jamais un artéfact d’un lieu archéologique ou historique. Les artéfacts appartiennent aux Yukonnais, et le gouvernement du Yukon en est le dépositaire. Pour signaler une découverte, appelez ',
                 contacts1: {
                     name1: 'Archéologie Yukon par téléphone au 867-667-5983',
                     // contact1: '867-667-5983',
@@ -230,7 +237,7 @@ export default {
                     // contact2: '1-800-661-0408',
                 },
                 title2: 'Règles de sécurité à suivre dans la nature',
-                content2: `Avant de partir à la découverte d’un sentier, renseignez-vous sur le secteur et faites-vous un plan. Au Yukon, de nombreux sentiers ont une longueur de plusieurs centaines de kilomètres.`,
+                content2: `Avant de partir à la découverte d’un sentier, renseignez-vous sur le secteur et ayez un plan. Au Yukon, de nombreux sentiers ont une longueur de plusieurs centaines de kilomètres.`,
                 url2: 'https://yukon.ca/fr/plein-air-faune-et-flore/securite-en-milieu-sauvage/se-deplacer-en-toute-securite-dans-la-nature',
                 url_text2: 'Consultez le site pour obtenir d’autres conseils sur le tourisme d’aventure.'
             }
