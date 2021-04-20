0e99b70aec7eb3cff996c11e0109e6d18b10a87f
Robert Ngo <robert.ngo@evolvingweb.ca>
2021-04-20 00:45:55 -0400

    refs #27428: fix reset filter.
    Former-commit-id: 3a5a5b9b00c2d7e1e8c9ebd550c45cc81934e468

--- src/screens/listing/index.js
+++ src/screens/listing/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {FlatList, TouchableOpacity, Dimensions} from 'react-native';
 
 import {NavigationEvents} from 'react-navigation';
