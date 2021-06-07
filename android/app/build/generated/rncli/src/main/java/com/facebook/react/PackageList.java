c2b5a7bbb72b1728e971372646f37dd0bd13249f
Robert Ngo <robert.ngo@evolvingweb.ca>
2021-06-07 09:41:13 -0400

    Make android build 6.
    Former-commit-id: 9e6ba18abc73dc6def7f86f517fb727ab78e4ebb

--- android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
+++ android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
@@ -11,9 +11,6 @@ import com.facebook.react.shell.MainReactPackage;
 import java.util.Arrays;
 import java.util.ArrayList;
 
-import com.yukon.BuildConfig;
-import com.yukon.R;
-
 // @react-native-community/masked-view
 import org.reactnative.maskedview.RNCMaskedViewPackage;
 // @react-native-community/netinfo
