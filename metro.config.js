509a052283062708149f3d600235a633027a7c4d
Robert Ngo <robert.ng.1125@gmail.com>
2022-03-07 21:55:01 -0500

    Update to latest react-native 0.67.3
    Former-commit-id: d8cc93a183fc3ad856063832835a2f5e7b94d65b

--- metro.config.js
+++ metro.config.js
@@ -1,21 +1,17 @@
-const srcDir = `${__dirname}/src`
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
 
 module.exports = {
   transformer: {
-    assetPlugins: ['expo-asset/tools/hashAssetFiles'],
-  },
-  resolver: {
-    extraNodeModules: {
-      '~app'        : srcDir,
-      '~components' : `${srcDir}/components`,
-      '~locale'     : `${srcDir}/locale`,
-      '~screens'    : `${srcDir}/screens`,
-      '~theme'      : `${srcDir}/theme`,
-      '~navigation' : `${srcDir}/navigation`,
-      '~shared'     : `${srcDir}/shared`,
-      '~store'      : `${srcDir}/store`,
-      '~assets'     : `${__dirname}/assets`,
-      '~swoosh'     : `${__dirname}/assets/images/swoosh`,
-    },
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
   },
 };
