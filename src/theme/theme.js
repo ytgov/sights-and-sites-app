394d61946e6845ce2e4fcc6219bd42793f5a5b65
Robert Ngo <robert.ng.1125@gmail.com>
2022-03-09 00:21:07 -0500

    Update Launchscreen

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,7 +1,5 @@
 export * from './typings';
 export * from './grid';
-// @ts-ignore
-export COMMON from './common';
-// @ts-ignore
-export Helpers from './helpers';
+export * from './common';
+export * from './helpers';
 export * from './layout';
