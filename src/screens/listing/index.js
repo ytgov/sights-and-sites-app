53a1a53c5bc5228ebf72836ed383c91e2dc25907
Robert Ngo <robert.ng.1125@gmail.com>
2022-03-08 23:32:26 -0500

    Listing
    Former-commit-id: 089450ff3702bd33959ec90169853944c643452f

--- src/screens/listing/index.js
+++ src/screens/listing/index.js
@@ -1,12 +1,16 @@
 import React, {useState, useEffect, useRef} from 'react';
-import {FlatList, TouchableOpacity, Dimensions} from 'react-native';
+import {FlatList, TouchableOpacity, Dimensions, Text} from 'react-native';
 
 import {NavigationEvents} from 'react-navigation';
 import {connect} from 'react-redux';
 import {useTranslation} from 'react-i18next';
 import {isNull as _isNull, isUndefined as _isUndefined} from 'lodash';
 
-import {hideSearch, setCurrentScreenName, showHeader} from '~store/actions/core';
+import {
+  hideSearch,
+  setCurrentScreenName,
+  showHeader,
+} from '~store/actions/core';
 import routes from '~navigation/routes';
 import HeaderNav, {HeaderNavType} from '~components/headerNav';
 import SiteCard from '~components/siteCard';
@@ -16,129 +20,139 @@ import DeviceInfo from 'react-native-device-info';
 import {YUKON_COLORS} from '~theme/config';
 import {filterListing, setListing} from '~store/actions/listing';
 
-const windowHeight = Dimensions.get('window').height
-const windowWidth = Dimensions.get('window').width
+const windowHeight = Dimensions.get('window').height;
+const windowWidth = Dimensions.get('window').width;
 
-const ITEMS_PER_PAGE = 10
+const ITEMS_PER_PAGE = 10;
 
-const ListingScreen = (props) => {
-    const {
-        navigation,
-        dispatchShowHeader,
-        dispatchHideSearch,
-        dispatchSetCurrentScreenName,
-        listingFiltered,
-        listingLocalized,
-        locale,
-        dispatchFilterListing,
-        dispatchSetListing
-    } = props
+const ListingScreen = props => {
+  const {
+    navigation,
+    dispatchShowHeader,
+    dispatchHideSearch,
+    dispatchSetCurrentScreenName,
+    listingFiltered,
+    listingLocalized,
+    locale,
+    dispatchFilterListing,
+    dispatchSetListing,
+  } = props;
 
-    const {t} = useTranslation()
-    const flatListRef = useRef();
-    const [items, setItems] = useState([])
-    const [page, setPage] = useState(1)
-    const [notification, setNotification] = useState(null)
-    const [didDispatch, setDidDispatch] = useState(false)
-    const maxPages = Math.ceil(listingFiltered.length / ITEMS_PER_PAGE)
+  const {t} = useTranslation();
+  const flatListRef = useRef();
+  const [items, setItems] = useState([]);
+  const [page, setPage] = useState(1);
+  const [notification, setNotification] = useState(null);
+  const [didDispatch, setDidDispatch] = useState(false);
+  const maxPages = Math.ceil(listingFiltered.length / ITEMS_PER_PAGE);
 
-    useEffect(() => {
-        if (!didDispatch) {
-            const list = listingLocalized[locale]
-            dispatchSetListing(list)
-            dispatchFilterListing()
-            setDidDispatch(true);
-            console.log('Move to top');
-            flatListRef.current.scrollToOffset({ animated: true, offset: 0 });
-        } else {
-            const loadedItems = listingFiltered.slice(0, ITEMS_PER_PAGE);
-            setItems(loadedItems);
-        }
-
-
-    }, [listingFiltered])
-
-    const loadMore = () => {
-        const newPage = page + 1
-        setPage(newPage)
-        if (newPage < maxPages) {
-            const newBatch = listingFiltered.slice(0, (page + 1) * ITEMS_PER_PAGE)
-            setItems(newBatch)
-        } else if (newPage === maxPages) {
-            setItems(listingFiltered)
-        }
+  useEffect(() => {
+    if (!didDispatch) {
+      const list = listingLocalized[locale];
+      dispatchSetListing(list);
+      dispatchFilterListing();
+      setDidDispatch(true);
+      flatListRef.current.scrollToOffset({animated: true, offset: 0});
+    } else {
+      const loadedItems = listingFiltered.slice(0, ITEMS_PER_PAGE);
+      setItems(loadedItems);
     }
+  }, [listingFiltered]);
 
-    return (
-        <>
-            <NavigationEvents
-                onWillFocus={() => {
-                    dispatchHideSearch();
-                    dispatchShowHeader();
-                    dispatchSetCurrentScreenName(null);
-                    setPage(1);
-                    flatListRef.current.scrollToOffset({ animated: true, offset: 0 });
+  const loadMore = () => {
+    const newPage = page + 1;
+    setPage(newPage);
+    if (newPage < maxPages) {
+      const newBatch = listingFiltered.slice(0, (page + 1) * ITEMS_PER_PAGE);
+      setItems(newBatch);
+    } else if (newPage === maxPages) {
+      setItems(listingFiltered);
+    }
+  };
 
-                    // Toast
-                    if (!_isUndefined(navigation.getParam('notification'))) {
-                        const toast = toastWithIcon(navigation.getParam('notification'), 'check-circle', {
-                            position: DeviceInfo.hasNotch() ? 110 : 80, /* StatusBar height + App menu height  */
-                            containerStyle: {
-                                paddingTop: 6,
-                                paddingBottom: 6,
-                                width: windowWidth,
-                                backgroundColor: YUKON_COLORS.primary,
-                            }
-                        }, ICON_POSITION.LEFT);
+  return (
+    <>
+      <NavigationEvents
+        onWillFocus={() => {
+          dispatchHideSearch();
+          dispatchShowHeader();
+          dispatchSetCurrentScreenName(null);
+          setPage(1);
+          flatListRef.current.scrollToOffset({animated: true, offset: 0});
 
-                        setNotification(toast)
-                    }
-                }}
-            />
+          // Toast
+          if (!_isUndefined(navigation.getParam('notification'))) {
+            const toast = toastWithIcon(
+              navigation.getParam('notification'),
+              'check-circle',
+              {
+                position: DeviceInfo.hasNotch()
+                  ? 110
+                  : 80 /* StatusBar height + App menu height  */,
+                containerStyle: {
+                  paddingTop: 6,
+                  paddingBottom: 6,
+                  width: windowWidth,
+                  backgroundColor: YUKON_COLORS.primary,
+                },
+              },
+              ICON_POSITION.LEFT,
+            );
 
-            <FlatList
-                style={{height: windowHeight}}
-                data={items}
-                ref={flatListRef}
-                initialNumToRender={3}
-                scrollEventThrottle={16}
-                renderItem={({item}) =>
-                    <TouchableOpacity activeOpacity={0.8}
-                                      onPress={() => navigation.navigate(routes.SCREEN_SITE_DETAILS, {site_id: item.site_id})}>
-                        <SiteCard data={item} />
-                    </TouchableOpacity>}
-                keyExtractor={(item) => item.site_id.toString()}
-                ListEmptyComponent={<NoResult />}
-                // On End Reached (Takes a function)
-                onEndReached={loadMore}
-                // Load more when reaching the last item
-                onEndReachedThreshold={0.5}
-            />
-        </>
-    );
+            setNotification(toast);
+          }
+        }}
+      />
+
+      <FlatList
+        style={{height: windowHeight}}
+        data={items}
+        ref={flatListRef}
+        initialNumToRender={3}
+        scrollEventThrottle={16}
+        renderItem={({item}) => (
+          <TouchableOpacity
+            activeOpacity={0.8}
+            onPress={() =>
+              navigation.navigate(routes.SCREEN_SITE_DETAILS, {
+                site_id: item.site_id,
+              })
+            }>
+            <SiteCard data={item} />
+          </TouchableOpacity>
+        )}
+        keyExtractor={item => item.site_id.toString()}
+        ListEmptyComponent={<NoResult />}
+        // On End Reached (Takes a function)
+        onEndReached={loadMore}
+        // Load more when reaching the last item
+        onEndReachedThreshold={0.5}
+      />
+    </>
+  );
 };
 
-ListingScreen['navigationOptions'] = () => ({
-    header: (props) => <HeaderNav {...props}
-                                  activeItem={HeaderNavType.LIST} />,
-})
+ListingScreen.navigationOptions = () => ({
+  header: props => <HeaderNav {...props} activeItem={HeaderNavType.LIST} />,
+});
 
-const mapStateToProps = (state) => {
-    return {
-        listingFiltered: state.listingStore.listingFiltered,
-        listingLocalized: state.listingStore.listingLocalized,
-        locale: state.localeStore.locale,
-    };
+const mapStateToProps = state => {
+  return {
+    listingFiltered: state.listingStore.listingFiltered,
+    listingLocalized: state.listingStore.listingLocalized,
+    locale: state.localeStore.locale,
+  };
 };
 
 const mapDispatchToProps = dispatch => {
-    return {
-        dispatchShowHeader: () => dispatch(showHeader()),
-        dispatchHideSearch: () => dispatch(hideSearch()),
-        dispatchSetCurrentScreenName: (value) => dispatch(setCurrentScreenName(value)),
-        dispatchSetListing: (list) => dispatch(setListing(list)),
-        dispatchFilterListing: () => dispatch(filterListing())
-    };
+  return {
+    dispatchShowHeader: () => dispatch(showHeader()),
+    dispatchHideSearch: () => dispatch(hideSearch()),
+    dispatchSetCurrentScreenName: value =>
+      dispatch(setCurrentScreenName(value)),
+    dispatchSetListing: list => dispatch(setListing(list)),
+    dispatchFilterListing: () => dispatch(filterListing()),
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ListingScreen);
--- src/theme/config.js
+++ src/theme/config.js
@@ -1,42 +1,42 @@
 const TYPINGS = {
-    h1: 34,
-    h2: 24,
-    h3: 22,
-    h4: 16,
-    body: 16,
-    small: 12,
+  h1: 34,
+  h2: 24,
+  h3: 22,
+  h4: 16,
+  body: 16,
+  small: 12,
 
-    subtitle1: 17,
-    subtitle2: 15,
-    body1: 16,
-    body2: 12,
-    caption: 12,
-    button: 14,
-    lineHeightOffset: 4
-}
+  subtitle1: 17,
+  subtitle2: 15,
+  body1: 16,
+  body2: 12,
+  caption: 12,
+  button: 14,
+  lineHeightOffset: 4,
+};
 
 const COLORS = {
-    primary: '#000000',
-    secondary: '#626366',
-    accent: '#F2A900',
-    warning: '#DB4514',
-    yellow: '#F3BA2F',
-    white: '#FFFFFF'
-}
+  primary: '#000000',
+  secondary: '#626366',
+  accent: '#F2A900',
+  warning: '#DB4514',
+  yellow: '#F3BA2F',
+  white: '#FFFFFF',
+};
 
 const YUKON_COLORS = {
-    primary: '#F2A900',
-    primary_100: '#7A9A01',
-    primary_200: '#0097A9',
-    primary_300: '#63A2AE',
-    primary_400: '#512A44',
-    primary_500: '#DE4300',
-    primary_600: '#224C5B',
-    neutral: '#000000',
-    neutral_100: '#444141',
-    neutral_200: '#CCCCCC',
-    neutral_300: '#DDDDDD',
-    neutral_400: '#F1F1F1',
-}
+  primary: '#F2A900',
+  primary_100: '#7A9A01',
+  primary_200: '#0097A9',
+  primary_300: '#63A2AE',
+  primary_400: '#512A44',
+  primary_500: '#DE4300',
+  primary_600: '#224C5B',
+  neutral: '#000000',
+  neutral_100: '#444141',
+  neutral_200: '#CCCCCC',
+  neutral_300: '#DDDDDD',
+  neutral_400: '#F1F1F1',
+};
 
-export {TYPINGS, COLORS, YUKON_COLORS}
+export {TYPINGS, COLORS, YUKON_COLORS};
