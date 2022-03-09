2396a056c64b76de6e607a570ac8d9132ab973d5
Robert Ngo <robert.ng.1125@gmail.com>
2022-03-08 22:55:56 -0500

    Fix navigation and typings
    Former-commit-id: 36fa11317efaa0fc2e18471980d1d6387d847222

--- src/navigation/routes.js
+++ src/navigation/routes.js
@@ -1,41 +1,41 @@
 const routes = {
-    STACK_BOTTOM_TAB: 'StackBottomTab',
+  STACK_BOTTOM_TAB: 'StackBottomTab',
 
-    STACK_MAIN: 'StackMain',
-    STACK_MODAL: 'StackModal',
-    STACK_DRAWER: 'StackDrawer',
-    STACK_APP_INSTRUCTION: 'StackIntro',
-    STACK_FILTERS: 'StackFilters',
-    STACK_SEARCH: 'StackSearch',
+  STACK_MAIN: 'StackMain',
+  STACK_MODAL: 'StackModal',
+  STACK_DRAWER: 'StackDrawer',
+  STACK_APP_INSTRUCTION: 'StackIntro',
+  STACK_FILTERS: 'StackFilters',
+  STACK_SEARCH: 'StackSearch',
 
-    SCREEN_LOADING: 'Loading',
-    SCREEN_WELCOME: 'Welcome',
-    SCREEN_INTRODUCTION: 'Introduction',
+  SCREEN_LOADING: 'Loading',
+  SCREEN_WELCOME: 'Welcome',
+  SCREEN_INTRODUCTION: 'Introduction',
 
-    SCREEN_LISTING: 'Listing',
-    SCREEN_MAP: 'Map',
-    SCREEN_SITE_DETAILS: 'SiteDetails',
-    SCREEN_SEARCH: 'Search',
-    SCREEN_HELPFUL_INFO: 'HelpfulInfo',
+  SCREEN_LISTING: 'Listing',
+  SCREEN_MAP: 'Map',
+  SCREEN_SITE_DETAILS: 'SiteDetails',
+  SCREEN_SEARCH: 'Search',
+  SCREEN_HELPFUL_INFO: 'HelpfulInfo',
 
-    SCREEN_CURRENT_CONDITIONS: 'CurrentConditions',
-    SCREEN_TRADITIONAL_TERRITORIES: 'TraditionalTerritories',
-    SCREEN_WILDERNESS_TRAVEL_TIPS: 'WildernessTravelTips',
-    SCREEN_APP_INFORMATION: 'AppInformation',
+  SCREEN_CURRENT_CONDITIONS: 'CurrentConditions',
+  SCREEN_TRADITIONAL_TERRITORIES: 'TraditionalTerritories',
+  SCREEN_WILDERNESS_TRAVEL_TIPS: 'WildernessTravelTips',
+  SCREEN_APP_INFORMATION: 'AppInformation',
 
-    SCREEN_APP_INSTRUCTIONS_1: 'IntroStepOne',
-    SCREEN_APP_INSTRUCTIONS_2: 'IntroStepTwo',
-    SCREEN_APP_INSTRUCTIONS_3: 'IntroStepThree',
-    SCREEN_APP_INSTRUCTIONS_4: 'IntroStepFour',
+  SCREEN_APP_INSTRUCTIONS_1: 'IntroStepOne',
+  SCREEN_APP_INSTRUCTIONS_2: 'IntroStepTwo',
+  SCREEN_APP_INSTRUCTIONS_3: 'IntroStepThree',
+  SCREEN_APP_INSTRUCTIONS_4: 'IntroStepFour',
 
-    SCREEN_FILTER_INDEX: 'FilterIndex',
-    SCREEN_FILTER_SITE_TYPE: 'FilterBySiteType',
-    SCREEN_FILTER_REGION: 'FilterByRegion',
-    SCREEN_FILTER_HIGHWAY: 'FilterByHighway',
-    SCREEN_FILTER_NEAR_ME: 'FilterNearMe',
-    SCREEN_FILTER_MY_FAVORITES: 'FilterMyFavorites',
+  SCREEN_FILTER_INDEX: 'FilterIndex',
+  SCREEN_FILTER_SITE_TYPE: 'FilterBySiteType',
+  SCREEN_FILTER_REGION: 'FilterByRegion',
+  SCREEN_FILTER_HIGHWAY: 'FilterByHighway',
+  SCREEN_FILTER_NEAR_ME: 'FilterNearMe',
+  SCREEN_FILTER_MY_FAVORITES: 'FilterMyFavorites',
 
-    SCREEN_SEARCH_RESULTS: 'SearchResults'
-}
+  SCREEN_SEARCH_RESULTS: 'SearchResults',
+};
 
 export default routes;
