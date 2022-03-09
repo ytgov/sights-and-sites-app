243eef410447025ee5817bf59d5e2eeed45e8aec
Robert Ngo <robert.ng.1125@gmail.com>
2022-03-08 22:55:56 -0500

    Fix navigation and typings
    Former-commit-id: d25eeea7a588d6af64a877f4df1aaeb7d8a36147

--- src/theme/typings.js
+++ src/theme/typings.js
@@ -5,14 +5,13 @@ import {COLORS, TYPINGS} from './config';
 const {lineHeightOffset} = TYPINGS;
 
 export const YUKON_FONTS = {
-  MONTSERRAT_REGULAR: 'Montserrat Regular',
-  MONTSERRAT_MEDIUM: 'Montserrat Medium',
-  MONTSERRAT_SEMI_BOLD: 'Montserrat SemiBold',
-  MONTSERRAT_BOLD: 'Montserrat SemiBold',
+  // MONTSERRAT_REGULAR: 'Montserrat Regular',
+  // MONTSERRAT_MEDIUM: 'Montserrat Medium',
+  // MONTSERRAT_SEMI_BOLD: 'Montserrat SemiBold',
+  // MONTSERRAT_BOLD: 'Montserrat SemiBold',
 };
 
 const H1 = styled.Text`
-  font-family: ${YUKON_FONTS.MONTSERRAT_BOLD};
   font-size: 36;
   line-height: 42;
   color: ${props => {
@@ -21,7 +20,6 @@ const H1 = styled.Text`
 `;
 
 const H2 = styled.Text`
-  font-family: ${YUKON_FONTS.MONTSERRAT_BOLD};
   font-size: 24;
   line-height: 28;
   color: ${props => {
@@ -30,7 +28,6 @@ const H2 = styled.Text`
 `;
 
 const H3 = styled.Text`
-  font-family: ${YUKON_FONTS.MONTSERRAT_BOLD};
   font-size: 22;
   line-height: 27;
   color: ${props => {
@@ -39,7 +36,6 @@ const H3 = styled.Text`
 `;
 
 const H4 = styled.Text`
-  font-family: ${YUKON_FONTS.MONTSERRAT_BOLD};
   font-size: 16;
   line-height: 20;
   color: ${props => {
@@ -48,15 +44,6 @@ const H4 = styled.Text`
 `;
 
 const Body = styled.Text`
-  font-family: ${props => {
-    if (props.fontBold) {
-      return YUKON_FONTS.MONTSERRAT_BOLD;
-    } else if (props.fontMedium) {
-      return YUKON_FONTS.MONTSERRAT_MEDIUM;
-    } else {
-      return YUKON_FONTS.MONTSERRAT_REGULAR;
-    }
-  }};
   font-size: 16;
   line-height: 24;
   color: ${props => {
@@ -65,12 +52,10 @@ const Body = styled.Text`
 `;
 
 const Small = styled.Text`
-  font-family: ${YUKON_FONTS.MONTSERRAT_MEDIUM}
-  font-size: 12
+  font-size: 12px;s
 `;
 
 const Subtitle1 = styled.Text`
-  font-family: ${YUKON_FONTS.MONTSERRAT_BOLD};
   font-size: ${TYPINGS.subtitle1};
   line-height: ${TYPINGS.subtitle1 + lineHeightOffset};
   color: ${props => {
@@ -79,7 +64,6 @@ const Subtitle1 = styled.Text`
 `;
 
 const Subtitle2 = styled.Text`
-  font-family: ${YUKON_FONTS.MONTSERRAT_BOLD};
   font-size: ${TYPINGS.subtitle2};
   line-height: ${TYPINGS.subtitle2 + lineHeightOffset};
   color: ${props => {
@@ -88,43 +72,143 @@ const Subtitle2 = styled.Text`
 `;
 
 const Body1 = styled.Text`
-  font-family: ${YUKON_FONTS.MONTSERRAT_SEMI_BOLD};
   font-size: ${TYPINGS.body1};
   line-height: ${TYPINGS.body1 + lineHeightOffset};
   color: ${props => {
     return props.black ? COLORS.primary : COLORS.white;
-  }} 
-  font-family: ${props => {
-    return props.bold
-      ? `${YUKON_FONTS.MONTSERRAT_BOLD}`
-      : props.regular
-      ? `${YUKON_FONTS.MONTSERRAT_REGULAR}`
-      : `${YUKON_FONTS.MONTSERRAT_SEMI_BOLD}`;
-  }} 
+  }};
 `;
 
 const Body2 = styled.Text`
-  font-family: ${YUKON_FONTS.MONTSERRAT_REGULAR};
-  font-family: ${props => {
-    return props.bold
-      ? `${YUKON_FONTS.MONTSERRAT_BOLD}`
-      : `${YUKON_FONTS.MONTSERRAT_REGULAR}`;
-  }}
   font-size: ${TYPINGS.body2};
   line-height: ${TYPINGS.body2 + lineHeightOffset};
   color: ${props => {
     return props.black ? COLORS.primary : COLORS.white;
-  }} 
+  }};
 `;
 
 const Caption = styled.Text`
-  font-family: ${YUKON_FONTS.MONTSERRAT_REGULAR};
   font-size: ${TYPINGS.caption};
   line-height: ${TYPINGS.caption + lineHeightOffset};
   color: ${props => {
     return props.black ? COLORS.primary : COLORS.white;
   }};
 `;
+
+// const H1 = styled.Text`
+//   font-family: ${YUKON_FONTS.MONTSERRAT_BOLD};
+//   font-size: 36;
+//   line-height: 42;
+//   color: ${props => {
+//     return props.black ? 'black' : 'white';
+// }};
+// `;
+//
+// const H2 = styled.Text`
+//   font-family: ${YUKON_FONTS.MONTSERRAT_BOLD};
+//   font-size: 24;
+//   line-height: 28;
+//   color: ${props => {
+//     return props.black ? 'black' : 'white';
+// }};
+// `;
+//
+// const H3 = styled.Text`
+//   font-family: ${YUKON_FONTS.MONTSERRAT_BOLD};
+//   font-size: 22;
+//   line-height: 27;
+//   color: ${props => {
+//     return props.black ? 'black' : 'white';
+// }};
+// `;
+//
+// const H4 = styled.Text`
+//   font-family: ${YUKON_FONTS.MONTSERRAT_BOLD};
+//   font-size: 16;
+//   line-height: 20;
+//   color: ${props => {
+//     return props.black ? 'black' : 'white';
+// }};
+// `;
+//
+// const Body = styled.Text`
+//   font-family: ${props => {
+//     if (props.fontBold) {
+//         return YUKON_FONTS.MONTSERRAT_BOLD;
+//     } else if (props.fontMedium) {
+//         return YUKON_FONTS.MONTSERRAT_MEDIUM;
+//     } else {
+//         return YUKON_FONTS.MONTSERRAT_REGULAR;
+//     }
+// }};
+//   font-size: 16;
+//   line-height: 24;
+//   color: ${props => {
+//     return props.black ? 'black' : 'white';
+// }};
+// `;
+//
+// const Small = styled.Text`
+//   font-family: ${YUKON_FONTS.MONTSERRAT_MEDIUM}
+//   font-size: 12
+// `;
+//
+// const Subtitle1 = styled.Text`
+//   font-family: ${YUKON_FONTS.MONTSERRAT_BOLD};
+//   font-size: ${TYPINGS.subtitle1};
+//   line-height: ${TYPINGS.subtitle1 + lineHeightOffset};
+//   color: ${props => {
+//     return props.black ? COLORS.primary : COLORS.white;
+// }};
+// `;
+//
+// const Subtitle2 = styled.Text`
+//   font-family: ${YUKON_FONTS.MONTSERRAT_BOLD};
+//   font-size: ${TYPINGS.subtitle2};
+//   line-height: ${TYPINGS.subtitle2 + lineHeightOffset};
+//   color: ${props => {
+//     return props.black ? COLORS.primary : COLORS.white;
+// }};
+// `;
+//
+// const Body1 = styled.Text`
+//   font-family: ${YUKON_FONTS.MONTSERRAT_SEMI_BOLD};
+//   font-size: ${TYPINGS.body1};
+//   line-height: ${TYPINGS.body1 + lineHeightOffset};
+//   color: ${props => {
+//     return props.black ? COLORS.primary : COLORS.white;
+// }}
+//   font-family: ${props => {
+//     return props.bold
+//         ? `${YUKON_FONTS.MONTSERRAT_BOLD}`
+//         : props.regular
+//             ? `${YUKON_FONTS.MONTSERRAT_REGULAR}`
+//             : `${YUKON_FONTS.MONTSERRAT_SEMI_BOLD}`;
+// }}
+// `;
+//
+// const Body2 = styled.Text`
+//   font-family: ${YUKON_FONTS.MONTSERRAT_REGULAR};
+//   font-family: ${props => {
+//     return props.bold
+//         ? `${YUKON_FONTS.MONTSERRAT_BOLD}`
+//         : `${YUKON_FONTS.MONTSERRAT_REGULAR}`;
+// }}
+//   font-size: ${TYPINGS.body2};
+//   line-height: ${TYPINGS.body2 + lineHeightOffset};
+//   color: ${props => {
+//     return props.black ? COLORS.primary : COLORS.white;
+// }}
+// `;
+//
+// const Caption = styled.Text`
+//   font-family: ${YUKON_FONTS.MONTSERRAT_REGULAR};
+//   font-size: ${TYPINGS.caption};
+//   line-height: ${TYPINGS.caption + lineHeightOffset};
+//   color: ${props => {
+//     return props.black ? COLORS.primary : COLORS.white;
+// }};
+// `;
 
 export {
   H1,
