565b537ce3724a7ac5e4a48f47a701e909885ba5
Robert Ngo <robert.ng.1125@gmail.com>
2022-03-09 23:18:36 -0500

    Add native-base, fix accordion.
    Former-commit-id: a797a84a2017e28f1a2839f46ede8e880845cd36

--- src/screens/helpfulInfo/wildernessTravelTips.js
+++ src/screens/helpfulInfo/wildernessTravelTips.js
@@ -1,59 +1,58 @@
-import React                    from 'react';
-import {useTranslation}         from 'react-i18next';
-import Section                  from '~screens/siteDetails/section';
-import HTMLElement              from '~components/htmlElement';
-import ScreenParallaxWrapper    from '~components/screenParallaxWrapper';
-import AccordionCustom          from '~components/accordion/accordion.component';
+import React from 'react';
+import {useTranslation} from 'react-i18next';
+import Section from '~screens/siteDetails/section';
+import HTMLElement from '~components/htmlElement';
+import ScreenParallaxWrapper from '~components/screenParallaxWrapper';
+import AccordionCustom from '~components/accordion/accordion.component';
 
 const bgWilderness = require('./images/bg-wilderness-travel-tips.png');
 const iconWilderness = require('./images/icon-wilderness.png');
 const swoosh = require('~swoosh/swoosh-silver-trail.png');
 
 const WildernessTravelTipsScreen = () => {
-    const { t } = useTranslation();
+  const {t} = useTranslation();
 
-    const accordionData = [
+  const accordionData = [
+    {
+      title: t('travelTips.data.title1'),
+      content_italicized: t('travelTips.data.content_italicized1'),
+      content: t('travelTips.data.content1'),
+      contacts: [
         {
-            title: t('travelTips.data.title1'),
-            content_italicized: t('travelTips.data.content_italicized1'),
-            content: t('travelTips.data.content1'),
-            contacts: [
-                {
-                    name: t('travelTips.data.contacts1.name1'),
-                    contact: t('travelTips.data.contacts1.contact1'),
-                },
-                {
-                    name: t('travelTips.data.contacts1.name2'),
-                    contact: t('travelTips.data.contacts1.contact2'),
-                }
-            ]
+          name: t('travelTips.data.contacts1.name1'),
+          contact: t('travelTips.data.contacts1.contact1'),
         },
         {
-            title: t('travelTips.data.title2'),
-            content: t('travelTips.data.content2'),
-            url: t('travelTips.data.url2'),
-        }
-    ];
+          name: t('travelTips.data.contacts1.name2'),
+          contact: t('travelTips.data.contacts1.contact2'),
+        },
+      ],
+    },
+    {
+      title: t('travelTips.data.title2'),
+      content: t('travelTips.data.content2'),
+      url: t('travelTips.data.url2'),
+    },
+  ];
 
-    return (
-        <ScreenParallaxWrapper backgroundImage={bgWilderness}
-                               swoosh={swoosh}
-                               leadIcon={iconWilderness}
-                               bookmarkButton={false}
-                               title={t('travelTips.title')}>
+  return (
+    <ScreenParallaxWrapper
+      backgroundImage={bgWilderness}
+      swoosh={swoosh}
+      leadIcon={iconWilderness}
+      bookmarkButton={false}
+      title={t('travelTips.title')}>
+      <Section title={t('travelTips.section.title')}>
+        <HTMLElement html={t('travelTips.section.stop')} />
+        <HTMLElement html={t('travelTips.section.look')} />
+        <HTMLElement html={t('travelTips.section.leave')} />
+      </Section>
 
-            <Section title={t('travelTips.section.title')}>
-                <HTMLElement html={t(`travelTips.section.stop`)} />
-                <HTMLElement html={t(`travelTips.section.look`)} />
-                <HTMLElement html={t(`travelTips.section.leave`)} />
-            </Section>
-
-            <Section>
-                <AccordionCustom data={accordionData}/>
-            </Section>
-
-        </ScreenParallaxWrapper>
-    );
+      <Section>
+        <AccordionCustom data={accordionData} />
+      </Section>
+    </ScreenParallaxWrapper>
+  );
 };
 
 export default WildernessTravelTipsScreen;
