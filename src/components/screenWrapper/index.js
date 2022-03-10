4b6b4a1a32c16ad3da6ab9dcaf45840787f01ebf
Robert Ngo <robert.ng.1125@gmail.com>
2022-03-09 22:42:01 -0500

    Refactor code.

--- src/components/screenWrapper/index.js
+++ src/components/screenWrapper/index.js
@@ -1,50 +1,57 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {ScrollView, View, ImageBackground, SafeAreaView, Text} from 'react-native';
+import {
+  ScrollView,
+  View,
+  ImageBackground,
+  SafeAreaView,
+  Text,
+} from 'react-native';
 
 import styles from './styles';
 
-const ScreenWrapper = ({children, backgroundImage, backgroundColor, footer}) => {
-    if (backgroundImage) {
-        return (
-            <View style={styles.wrapper}>
-                <ImageBackground source={backgroundImage} style={styles.background}>
-                    <View style={styles.overlay} />
-                    <ScrollView contentContainerStyle={{flexGrow: 1}}>
-                        <View style={styles.inner}>
-                            {children}
-                        </View>
-                    </ScrollView>
-                    {footer}
-                </ImageBackground>
-            </View>
-        );
-    } else  {
-        return (
-            <SafeAreaView style={{ flex: 1 }}>
-                <View style={[styles.wrapper, {backgroundColor}]}>
-                    <ScrollView>
-                        <View style={styles.inner}>
-                            {children}
-                        </View>
-                    </ScrollView>
-                    {footer}
-                </View>
-            </SafeAreaView>
-        )
-    }
+const ScreenWrapper = ({
+  children,
+  backgroundImage,
+  backgroundColor,
+  footer,
+}) => {
+  if (backgroundImage) {
+    return (
+      <View style={styles.wrapper}>
+        <ImageBackground source={backgroundImage} style={styles.background}>
+          <View style={styles.overlay} />
+          <ScrollView contentContainerStyle={{flexGrow: 1}}>
+            <View style={styles.inner}>{children}</View>
+          </ScrollView>
+          {footer}
+        </ImageBackground>
+      </View>
+    );
+  } else {
+    return (
+      <SafeAreaView style={{flex: 1}}>
+        <View style={[styles.wrapper, {backgroundColor}]}>
+          <ScrollView>
+            <View style={styles.inner}>{children}</View>
+          </ScrollView>
+          {footer}
+        </View>
+      </SafeAreaView>
+    );
+  }
 };
 
 ScreenWrapper.propTypes = {
-    backgroundImage: PropTypes.node,
-    backgroundColor: PropTypes.string,
-    footer: PropTypes.node
-}
+  backgroundImage: PropTypes.node,
+  backgroundColor: PropTypes.string,
+  footer: PropTypes.node,
+};
 
 ScreenWrapper.defaultProps = {
-    backgroundImage: null,
-    backgroundColor: 'white',
-    footer: null
-}
+  backgroundImage: null,
+  backgroundColor: 'white',
+  footer: null,
+};
 
 export default ScreenWrapper;
