a797a84a2017e28f1a2839f46ede8e880845cd36
Robert Ngo <robert.ng.1125@gmail.com>
2022-03-09 23:18:36 -0500

    Add native-base, fix accordion.

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {Provider} from 'react-redux';
 import {RootSiblingParent} from 'react-native-root-siblings';
 import NetworkGate from './src/shared/components/netwrok-gate/netwrok-gate.component';
-
 import configureStore from './src/store';
 import AppRoot from './src';
 import {PersistGate} from 'redux-persist/integration/react';
