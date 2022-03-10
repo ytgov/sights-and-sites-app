4d35a4196bed7bc7fba988b9ca2d7b4047b3203d
Robert Ngo <robert.ng.1125@gmail.com>
2022-03-09 22:33:41 -0500

    Add RootSiblingParent, fix toast

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Provider} from 'react-redux';
+import {RootSiblingParent} from 'react-native-root-siblings';
 import NetworkGate from './src/shared/components/netwrok-gate/netwrok-gate.component';
 
 import configureStore from './src/store';
@@ -13,7 +14,9 @@ const App = () => {
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <NetworkGate>
-          <AppRoot />
+          <RootSiblingParent>
+            <AppRoot />
+          </RootSiblingParent>
         </NetworkGate>
       </PersistGate>
     </Provider>
