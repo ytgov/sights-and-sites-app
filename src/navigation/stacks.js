45a4d9b5f3bb5696b76c45914c1ff67dad138659
Ruslan Nigmatulin <avadon.90@gmail.com>
2022-07-01 17:28:00 +0400

    Patched react-navigation-drawer + minor fixes
    Former-commit-id: f3112cd6351a5c6b9ee6cba8d383c7479ba97a62

--- src/navigation/stacks.js
+++ src/navigation/stacks.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Dimensions, Text} from 'react-native';
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
-import {createDrawerNavigator} from 'react-navigation-drawer';
+import {createDrawerNavigator} from '@avadon/react-navigation-drawer';
 import {createStackNavigator} from 'react-navigation-stack';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
 
